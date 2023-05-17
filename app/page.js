eff760aa4787ac35b984602c0d3edfce7fd9a6c9
vikassics <vikas@joinnextmed.com>
2023-05-17 16:01:40 +0530

    added desktop

--- app/page.js
+++ app/page.js
@@ -1,15 +1,15 @@
 
-import Direction from "./components/Direction-logo/Direction";
+import Direction from "@/app/components/Abovefold/Above";
 import Industry from "./components/Industry-Experts/Industry";
 import Info from "./components/Info-Screen/Info";
 import Trail from "./components/Trail/Trail";
 import Benifits from "./components/Benifits/Benifits";
-import AboveFold from "./components/Abovefold/Fold";
+import Above from "./components/Abovefold/Above";
 
 export default function Home() {
   return (
     <main className="">
-      <AboveFold />
+      <Above/>
       <Direction />
       <Info />
       <Benifits />
