369af3fda9538f464e5c225ceca09f5a7c5f2010
vikassics <vikas@joinnextmed.com>
2023-05-17 15:43:34 +0530

    added desktop

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
-import Fold from "./components/Abovefold/Fold";
+
 import Direction from "./components/Direction-logo/Direction";
 import Industry from "./components/Industry-Experts/Industry";
 import Info from "./components/Info-Screen/Info";
 import Trail from "./components/Trail/Trail";
 import Benifits from "./components/Benifits/Benifits";
+import Fold from "./components/Abovefold/Fold";
 
 export default function Home() {
   return (
