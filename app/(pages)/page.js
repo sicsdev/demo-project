d24e976f98cb7ae00cc7be21faac12faf4464ea1
joywithyashh <yash@usetempo.ai>
2023-06-01 20:57:58 +0530

    merged ui + functional changes

--- app/(pages)/page.js
+++ app/(pages)/page.js
@@ -3,19 +3,29 @@ import Info from "../components/Info-Screen/Info";
 import Trial from "../components/Trial/Trial";
 import Benifits from "../components/Benifits/Benifits";
 import Start from "../components/Get-Start/Start";
-import Ecommerce from "../components/Ecommerce/Ecommerce";
 import Testimonial from "../components/Testimonial/Testimonial";
 import DTC from "../components/DTC/DTC";
+import Head from "next/head";
+import SecondBan from "../components/Layout/SecondBan";
+import Social from "../components/Social/Social";
+import Faq from "../components/Faq/Faq";
+import Newstandard from "../components/Newstandardpage/Newstandard";
+import Brandpercentage from "../components/Brandpercentage/Brandpercentage";
 export default function Home() {
   return (
     <main className="scroll-smooth">
-      <Start />``
+      <Head></Head>
+      <Start />
       <DTC />
       <Info />
-      <Benifits />
-      <Ecommerce />
-      <Testimonial />
       <Trial />
+      <SecondBan />
+      <Benifits />        {/* new */}
+      <Brandpercentage/>  {/* new */}
+      <Social />
+      <Testimonial />
+      <Newstandard/>      {/* new */}
+      <Faq />             {/* new */}
     </main>
-  )
+  );
 }
