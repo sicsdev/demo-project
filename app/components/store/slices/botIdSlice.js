d7f9e80a337c1e23793a116434ccfba9fcd4a235
joywithyashh <134736071+joywithyashh@users.noreply.github.com>
2023-06-01 05:58:41 +0530

    Update botIdSlice.js

--- app/components/store/slices/botIdSlice.js
+++ app/components/store/slices/botIdSlice.js
@@ -2,7 +2,7 @@ const { createSlice } = require("@reduxjs/toolkit");
 
 export const botIdSlice = createSlice({
     name: "bot_id",
-    initialState: { id: '240a4e82-3c9f-4fbc-b130-751027daa87a' }
+    initialState: { id: null }
     ,
     reducers: {
         setBotId: (state, action) => {
