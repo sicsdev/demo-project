527fd59996e41ec1887f73e4df3db6b187c3846f
joywithyashh <yash@usetempo.ai>
2023-06-03 00:45:48 +0530

    memberschanges

--- app/components/store/store.js
+++ app/components/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import versionReducer from './slices/versionSlice';
 import botIdReducer from './slices/botIdSlice';
 import userReducer from './slices/userSlice';
+import memberReducer from './slices/memberSlice';
 const store = configureStore({
     reducer: {
         version: versionReducer,
         botId: botIdReducer,
         user: userReducer,
+        members: memberReducer,
     },
 });
 
