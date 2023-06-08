21cf0e1e1bb7aa0dd714fa31f53ff22547d96f67
vivekTempo <vivek@useTempo.ai>
2023-06-09 01:06:50 +0530

    add file

--- app/(pages)/help/page.js
+++ app/(pages)/help/page.js
@@ -216,8 +216,8 @@ const Help = () => {
                 >
                   <div className="flex flex-col	justify-center w-[100%]">
                     {searchResults.map((ele, key) => (
-                      <Link href={`help/${ele.first_slug + "" + ele.slug}` key={key}>
-                        <div className="mb-14 shadow p-5 ">
+                      <Link href={`help/${ele.first_slug + "" + ele.slug}`}>
+                        <div className="mb-14 shadow p-5 " key={key}>
                           <h2 className="font-semibold text-2xl md:text-2xl lg:text-2xl sm:text-2xl text-black mb-3 js-show-on-scroll">
                             {ele.name}
                           </h2>
