2076e48a626c05ee1ec85ec8522410db8de31ddc
zkcastNM <134722001+zkcastTMP@users.noreply.github.com>
2023-06-08 14:51:40 -0300

    Fix: Banner text responsive

--- app/components/Layout/Banner.js
+++ app/components/Layout/Banner.js
@@ -3,18 +3,18 @@ import Link from "next/link";
 const Banner = () => {
   return (
     <div className="z-40 text-center p-3 bg-[#142543]">
-      <h6 className="text-blue-400 text-white xs:flex-row xs:flex-col sm:flex justify-center gap-2">
+      <h5 class="text-blue-400 text-white text-xs sm:text-base md:text-base">
         Onboard real agents powered by ChatGPT to plug into Shopify, Salesforce,
         Zendesk, or your custom store.{" "}
         {/* <span className="underline cursor-pointer "> Start Now</span> */}
         <span className="mt-2 sm:mt-0 inline pl-2 underline "
-        
+
         >
-          <Link  href='/free-trial'>
-           Start Now
-           </Link>
-           </span>
-      </h6>
+          <Link href='/free-trial'>
+            Start Now
+          </Link>
+        </span>
+      </h5>
     </div>
   );
 };
