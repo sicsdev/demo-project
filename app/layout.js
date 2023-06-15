665aab8693bad9e93960782888fa1ef9c9674af7
vivekTempo <vivek@useTempo.ai>
2023-06-16 03:54:14 +0530

    add GA script

--- app/layout.js
+++ app/layout.js
@@ -67,6 +67,17 @@ export default function RootLayout({ children }) {
 `,
           }}
         />
+{/* <!-- Google tag (gtag.js) --> */}
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-GEYJNVQCQE"></script>
+<script  
+ dangerouslySetInnerHTML={{
+  __html:  `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-GEYJNVQCQE')`
+ }}
+/>
 
         <script
           async
