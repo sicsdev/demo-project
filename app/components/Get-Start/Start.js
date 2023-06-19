270ae4e0ce74bb8434e3b604e3911dc07e45c26c
vivekTempo <vivek@useTempo.ai>
2023-06-19 17:55:01 +0530

    design updates

--- app/components/Get-Start/Start.js
+++ app/components/Get-Start/Start.js
@@ -11,13 +11,13 @@ const Start = () => {
     ]
 
 useEffect(()=>{
-    if (navigator.userAgent.match(/Android/i)
-    || navigator.userAgent.match(/webOS/i)
-    || navigator.userAgent.match(/iPhone/i)
-    || navigator.userAgent.match(/iPad/i)
-    || navigator.userAgent.match(/iPod/i)
-    || navigator.userAgent.match(/BlackBerry/i)
-    || navigator.userAgent.match(/Windows Phone/i)) {
+    if (window.navigator.userAgent.match(/Android/i)
+    || window.navigator.userAgent.match(/webOS/i)
+    || window.navigator.userAgent.match(/iPhone/i)
+    || window.navigator.userAgent.match(/iPad/i)
+    || window.navigator.userAgent.match(/iPod/i)
+    || window.navigator.userAgent.match(/BlackBerry/i)
+    || window.navigator.userAgent.match(/Windows Phone/i)) {
        setShow(false);
     } else {
         setShow(true);
