33aded53e6aa033b85ed39aea6a6fb8adc6f3ad4
zkcastNM <134722001+zkcastTMP@users.noreply.github.com>
2023-06-19 17:41:28 -0300

    Fix: Cookies delete on logout

--- app/components/Dashboard/AuthLayout/Sidebar.js
+++ app/components/Dashboard/AuthLayout/Sidebar.js
@@ -32,9 +32,21 @@ const Sidebar = ({ children }) => {
 
     const handleLogout = () => {
         localStorage.removeItem('Token');
+        clearCookies()
         router.push('/');
     };
 
+    const clearCookies = () => {
+        console.log(document.cookie)
+        const cookies = document.cookie.split(";")
+        console.log(cookies)
+        for (var i = 0; i < cookies.length; i++) {
+          var cookie = cookies[i];
+          var eqPos = cookie.indexOf("=");
+          var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+          document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT"
+        }
+      }
 
     const SideBarRoutes = [
         {
