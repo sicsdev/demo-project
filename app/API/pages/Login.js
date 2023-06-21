446d3d9ef7e87fc7432f7752063d99bcb1ddb5df
zkcastNM <134722001+zkcastTMP@users.noreply.github.com>
2023-06-21 17:36:26 -0300

    Add: Google Auth

--- app/API/pages/Login.js
+++ app/API/pages/Login.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import getConfig from 'next/config';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
@@ -12,3 +13,36 @@ export const submitLogin = async (body) => {
 };
 
 
+export const exchangeGoogleToken = async (body) => {
+
+    const config = { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+
+    try {
+        const response = await axios.post(`https://tempouserpool.auth.eu-north-1.amazoncognito.com/oauth2/token`, body, config);
+        return response.data;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// • Instructions for exchangeGoogleToken body:
+// client_id: The client ID provided by Amazon Cognito.
+// client_secret: The client secret provided by Amazon Cognito.
+// redirect_uri: Redirect URI.
+// grant_type: Set it to "authorization_code".
+// code: The authorization code received in the query parameters of the redirect URI.
+
+
+// to get user info: https://tempouserpool.auth.eu-north-1.amazoncognito.com/oauth2/userInfo
+
+export const getUserInfoFromCognito = async (token) => {
+    
+        const config = { headers: { 'Authorization': `Bearer ${token}` } }
+    
+        try {
+            const response = await axios.get(`https://tempouserpool.auth.eu-north-1.amazoncognito.com/oauth2/userInfo`, config);
+            return response.data;
+        } catch (error) {
+            console.log(error)
+        }
+    }
