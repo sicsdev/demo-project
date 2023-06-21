a1a9209526658535ab7494ced3c7e57d9559792f
vivekTempo <vivek@useTempo.ai>
2023-06-22 04:39:54 +0530

    cha bots pages

--- app/components/ChatBots/Alert.js
+++ app/components/ChatBots/Alert.js
@@ -20,7 +20,7 @@ const Alert = () => {
                 />
               </div>
               <div>
-                <h3 className="font-bold  text-2xl  md:text-h5 lg:text-h5 sm:text-h5    text-heading">
+                <h3 className="font-bold  text-[18px] md:text-h5 lg:text-h5 sm:text-h5    text-heading">
                   Automatically alert relationship owners
                 </h3>
                 <p className="">
@@ -41,7 +41,7 @@ const Alert = () => {
                 />
               </div>
               <div>
-                <h3 className="font-bold text-2xl md:text-h5 lg:text-h5 sm:text-h5 text-heading">
+                <h3 className="font-bold text-[18px]  md:text-h5 lg:text-h5 sm:text-h5 text-heading">
                   See risks well before "cancel"
                 </h3>
                 <p className="">
@@ -65,8 +65,8 @@ const Alert = () => {
         </div>
       </div>
 
-      <div className="max-w-[1400px] w-full m-auto sm:py-8 md:py-8 lg:py-8 px-0 sm:px-4 lg:px-4 relative group">
-        <h2 className="font-bold  text-2xl  md:text-h2 lg:text-h2 sm:text-h2 text-center  sm:my-8  text-heading">
+      <div className="max-w-[1400px] w-full py-5 m-auto sm:py-8 md:py-8 lg:py-8 px-0 sm:px-4 lg:px-4 relative group sm:bg-white bg-background">
+        <h2 className="font-bold  text-2xl  md:text-h2 lg:text-h2 sm:text-h2 text-center  sm:my-8 text-[#8ed1fc] sm:text-heading">
           Alert Relationship Owners to Churn Risks
         </h2>
         <div className="block sm:flex md:flex lg:flex justify-evenly gap-10">
@@ -79,7 +79,7 @@ const Alert = () => {
               />
             </div>
           </div>
-          <div className="p-4 sm:p-28">
+          <div className="p-4 sm:p-28 ">
             <div className="flex justify-start gap-6">
               <div className="relative w-[90px] h-[35px]">
                 <Image
@@ -90,10 +90,10 @@ const Alert = () => {
                 />
               </div>
               <div>
-                <h3 className="font-bold  text-2xl  md:text-h5 lg:text-h5 sm:text-h5    text-heading">
+                <h3 className="font-bold  text-[18px]   md:text-h5 lg:text-h5 sm:text-h5   text-white sm:text-heading">
                   Automatically alert relationship owners
                 </h3>
-                <p className="">
+                <p className="text-white sm:text-heading">
                   Automatically alert relationship owners Nothing slips through
                   the cracks or gets lost in translation. Natural language AI
                   detects negative language so you can get everyone on the same
@@ -101,7 +101,7 @@ const Alert = () => {
                 </p>
               </div>
             </div>
-            <div className="flex justify-start gap-6 mt-6">
+            <div className="flex justify-start gap-6 mt-6  ">
               <div className="relative w-[90px] h-[35px]">
                 <Image
                   fill={true}
@@ -111,10 +111,10 @@ const Alert = () => {
                 />
               </div>
               <div>
-                <h3 className="font-bold text-2xl md:text-h5 lg:text-h5 sm:text-h5    text-heading">
+                <h3 className="font-bold text-[18px] md:text-h5 lg:text-h5 sm:text-h5   text-white sm:text-heading">
                   See risks well before "cancel"
                 </h3>
-                <p className="">
+                <p className="text-white sm:text-heading">
                   Automatically alert relationship owners Nothing slips through
                   the cracks or gets lost in translation. Natural language AI
                   detects negative language so you can get everyone on the same
--- app/components/Layout/Nav.js
+++ app/components/Layout/Nav.js
@@ -15,84 +15,84 @@ const Nav = () => {
 
   return (
     <>
-        <nav className="sticky top-0 start-0 z-[999999] sm:z-50 w-full  shadow-xl bg-heading border-gray-200 ">
-          <Banner />
+      <nav className="sticky top-0 start-0 z-[999999] sm:z-50 w-full  shadow-xl bg-heading border-gray-200 ">
+        <Banner />
 
-          <div className="flex-wrap flex md:flex sm:flex lg:flex  items-center  h-[60px]">
-            <div className="relative flex flex-row items-center w-full px-6 sm:px-12 md:px-12 lg:px-12 h-[60px]">
-              <div className="relative w-28 h-8 mr-24">
-                <Link href="/">
-                  <Image
-                    fill={"true"}
-                    className="bg-contain mx-auto w-full"
-                    alt="logo.png"
-                    src={"/logo.png"}
-                  />
-                </Link>
-              </div>
-              <ul className="hidden relative md:flex text-white gap-8 flex-row">
-                {nav_links.map((element, key) => (
-                  <li
-                    key={key}
-                    className="py-[15px] h-[60px] group relative cursor-pointer hover:border hover:border-b-white hover:border-r-0 hover:border-l-0 hover:border-t-0 "
-                  >
-                    <Link href={element.link}>{element.name}</Link>
-                    {element.card.links.length > 0 && (
-                      <Card
-                        className={
-                          "animate-fadeIn w-[800px] hidden group-hover:block absolute top-[61px] bg-white"
-                        }
-                      >
-                        <List
-                          className={"grid grid-cols-2 gap-8"}
-                          nav_links={element.card.links}
-                        />
-                      </Card>
-                    )}
-                  </li>
-                ))}
-              </ul>
-              <div className="hidden md:flex flex-row gap-10 items-center ml-auto">
-                <Link href={"/login"}>
-                  {" "}
-                  <p className="text-white">Sign In</p>
-                </Link>
-                <Link href={"/free-trial"}>
-                  {" "}
-                  <button
-                    type="button"
-                    className="inline-block  bg-white px-6 pb-2 pt-2.5 text-xs rounded-2xl font-medium uppercase leading-normal text-heading shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition duration-150 border ease-in-out hover:bg-neutral-800 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-white focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] "
-                  >
-                    Get Started
-                  </button>{" "}
-                </Link>
-              </div>
-              <div className="flex md:hidden flex-row relative ml-auto cursor-pointer">
-                {show === false ? (
-                  <Bars4Icon
-                    className="animate-fadeIn h-8 w-8 text-white"
-                    onClick={(e) => {
-                      setShow(true);
-                    }}
-                  />
-                ) : (
-                  <XMarkIcon
-                    className="animate-fadeIn h-8 w-8 text-white"
-                    onClick={(e) => {
-                      setShow(false);
-                    }}
-                  />
-                )}
-              </div>
+        <div className="flex-wrap flex md:flex sm:flex lg:flex  items-center  h-[60px]">
+          <div className="relative flex flex-row items-center w-full px-6 sm:px-12 md:px-12 lg:px-12 h-[60px]">
+            <div className="relative w-28 h-8 mr-24">
+              <Link href="/">
+                <Image
+                  fill={"true"}
+                  className="bg-contain mx-auto w-full"
+                  alt="logo.png"
+                  src={"/logo.png"}
+                />
+              </Link>
+            </div>
+            <ul className="hidden relative md:flex text-white gap-8 flex-row">
+              {nav_links.map((element, key) => (
+                <li
+                  key={key}
+                  className="py-[15px] h-[60px] group relative cursor-pointer hover:border hover:border-b-white hover:border-r-0 hover:border-l-0 hover:border-t-0 "
+                >
+                  <Link href={element.link}>{element.name}</Link>
+                  {element.card.links.length > 0 && (
+                    <Card
+                      className={
+                        "animate-fadeIn w-[800px] hidden group-hover:block absolute top-[61px] bg-white"
+                      }
+                    >
+                      <List
+                        className={"grid grid-cols-2 gap-8"}
+                        nav_links={element.card.links}
+                      />
+                    </Card>
+                  )}
+                </li>
+              ))}
+            </ul>
+            <div className="hidden md:flex flex-row gap-10 items-center ml-auto">
+              <Link href={"/login"}>
+                {" "}
+                <p className="text-white">Sign In</p>
+              </Link>
+              <Link href={"/free-trial"}>
+                {" "}
+                <button
+                  type="button"
+                  className="inline-block  bg-white px-6 pb-2 pt-2.5 text-xs rounded-2xl font-medium uppercase leading-normal text-heading shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition duration-150 border ease-in-out hover:bg-neutral-800 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-white focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] "
+                >
+                  Get Started
+                </button>{" "}
+              </Link>
+            </div>
+            <div className="flex md:hidden flex-row relative ml-auto cursor-pointer">
+              {show === false ? (
+                <Bars4Icon
+                  className="animate-fadeIn h-8 w-8 text-white"
+                  onClick={(e) => {
+                    setShow(true);
+                  }}
+                />
+              ) : (
+                <XMarkIcon
+                  className="animate-fadeIn h-8 w-8 text-white"
+                  onClick={(e) => {
+                    setShow(false);
+                  }}
+                />
+              )}
             </div>
           </div>
+        </div>
 
-          {show === true && (
-            <div className="block md:hidden lg:hidden sm:hidden w-full ">
-              <Accordian setShow={setShow} nav_links={nav_links} />
-            </div>
-          )}
-        </nav>
+        {show === true && (
+          <div className="block md:hidden lg:hidden sm:hidden w-full ">
+            <Accordian setShow={setShow} nav_links={nav_links} />
+          </div>
+        )}
+      </nav>
     </>
   );
 };
