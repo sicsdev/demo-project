b4aab08ea42668b2df9867d00b73dde2be42efe3
joywithyashh <yash@usetempo.ai>
2023-06-21 21:20:41 +0530

    blog fix

--- next.config.js
+++ next.config.js
@@ -1,18 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['dam.freshworks.com', 'www.freshworks.com', 'assets-global.website-files.com', 'static.intercomassets.com', 'res.cloudinary.com', 'randomuser.me'],
-        unoptimized: true,
-    },
-    reactStrictMode: true,
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    output: 'export',
- 
-                source: '/robots.txt',
-                destination: '/api/robots'
-            
-}
+  images: {
+    domains: [
+      "dam.freshworks.com",
+      "www.freshworks.com",
+      "assets-global.website-files.com",
+      "static.intercomassets.com",
+      "res.cloudinary.com",
+      "randomuser.me",
+    ],
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/robots.txt",
+        destination: "/api/robots",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
