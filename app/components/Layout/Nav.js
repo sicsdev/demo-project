d1e7e16ad8909ed3a409e3380ee204bbcaa1d34c
zkcastTMP <134722001+zkcastTMP@users.noreply.github.com>
2023-06-30 19:25:16 +0200

    add: verify email banner

--- app/components/Layout/Nav.js
+++ app/components/Layout/Nav.js
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 import Card from "../Common/Card/Card";
 import List from "./components/List";
 import { Bars4Icon, XMarkIcon } from "@heroicons/react/24/outline";
@@ -9,15 +9,26 @@ import { useState } from "react";
 import Link from "next/link";
 import { nav_links } from "./data/navData";
 import Banner from "./Banner";
+import { getUserProfile } from "@/app/API/components/Sidebar";
+import VerifyEmailBanner from "./VerifyEmailBanner";
 
 const Nav = () => {
   const [show, setShow] = useState(false);
+  const [profile, setProfile] = useState({});
+  useEffect(() => {
+    getUserProfile().then((res) => {
+      setProfile(res);
+      console.log(res)
+    }).catch((err) => {
+      console.log(err);
+    })
+  }, []);
 
   return (
     <>
       <nav className="sticky top-0 start-0 z-[999999] sm:z-50 w-full  shadow-xl bg-heading border-gray-200 ">
-        <Banner />
-
+        {!profile.email && <Banner />}
+        {!profile.verified && <VerifyEmailBanner />}
         <div className="flex-wrap flex md:flex sm:flex lg:flex  items-center  h-[60px]">
           <div className="relative flex flex-row items-center w-full px-6 sm:px-12 md:px-12 lg:px-12 h-[60px]">
             <div className="relative w-28 h-8 mr-24">
@@ -53,19 +64,41 @@ const Nav = () => {
               ))}
             </ul>
             <div className="hidden md:flex flex-row gap-10 items-center ml-auto">
-              <Link href={"/login"}>
-                {" "}
-                <p className="text-white">Sign In</p>
-              </Link>
-              <Link href={"/free-trial"}>
-                {" "}
-                <button
-                  type="button"
-                  className="inline-block  bg-white px-6 pb-2 pt-2.5 text-xs rounded-2xl font-medium uppercase leading-normal text-heading shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition duration-150 border ease-in-out hover:bg-neutral-800 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-white focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] "
-                >
-                  Get Started
-                </button>{" "}
-              </Link>
+
+              {profile.email ?
+                <>
+                  {" "}
+                  <p className="text-white">{profile.email}</p>
+                </>
+                :
+                <Link href={"/login"}>
+                  {" "}
+                  <p className="text-white">Sign In</p>
+                </Link>
+              }
+
+              {profile.email ?
+                <Link href={"/dashboard"}>
+                  {" "}
+                  <button
+                    type="button"
+                    className="inline-block  bg-white px-6 pb-2 pt-2.5 text-xs rounded-2xl font-medium uppercase leading-normal text-heading shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition duration-150 border ease-in-out hover:bg-neutral-800 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-white focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] "
+                  >
+                    Dashboard
+                  </button>{" "}
+                </Link>
+                :
+                <Link href={"/free-trial"}>
+                  {" "}
+                  <button
+                    type="button"
+                    className="inline-block  bg-white px-6 pb-2 pt-2.5 text-xs rounded-2xl font-medium uppercase leading-normal text-heading shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition duration-150 border ease-in-out hover:bg-neutral-800 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-white focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] "
+                  >
+                    Get Started
+                  </button>{" "}
+                </Link>
+              }
+
             </div>
             <div className="flex md:hidden flex-row relative ml-auto cursor-pointer">
               {show === false ? (
