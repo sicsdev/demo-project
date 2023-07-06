f8b762665ed38d2f6d70b17d679b48d4dbba34c9
vivekTempo <vivek@useTempo.ai>
2023-07-07 04:13:29 +0530

    add component

--- app/components/solutions/Tempoeconomy.js
+++ app/components/solutions/Tempoeconomy.js
@@ -3,40 +3,32 @@ import React from "react";
 const Tempoeconomy = () => {
   return (
     <div className="bg-white">
-      <div className=" mx-auto max-w-[90%] sm:max-w-[80%] md:max-w-[80%] lg:max-w-[80%]  py-10">
-      <div className="sm:max-w-[50%] w-full">
-            <h6 class="font-bold text-xl black py-1 text-primary">Pricing</h6>
-            <h1 class="text-left text-2xl tracking-wide text-heading sm:text-3xl md:text-4xl lg:text-4xl my-2 font-bold ">
-              Give creators new ways to earn money on your platform{" "}
-            </h1>
-            <p className="sm:mt-4 sm:mb-3 mb-3">
-              Stripe’s products make it easy for creators to earn more revenue
-              on your platform with minimal engineering, legal, and compliance
-              investment. Help creators build stronger relationships with their
-              fans by allowing them to accept payments from around the world in
-              exchange for sharing their content and craft. You can also help
-              creators manage their cash flow by issuing credit cards and
-              digital wallets.
-            </p>
-          </div>
+      <div className=" mx-auto max-w-[90%] sm:max-w-[90%]   py-10">
+        <div className="sm:max-w-[50%] w-full">
+          <h6 class="font-bold text-xl black py-1 text-primary">Pricing</h6>
+          <h1 class="text-left text-2xl tracking-wide text-heading sm:text-3xl md:text-4xl lg:text-4xl my-2 font-bold ">
+            Optimize Your Customer Support with Tempo Chat{" "}
+          </h1>
+          <p className="sm:mt-4 sm:mb-3 mb-3">
+            Empower your platform with an AI that never sleeps. Tempo Chat helps
+            you efficiently tackle complex support tickets, allowing you to
+            focus on improving your customer journey. Cut your customer service
+            costs and pay only for what you use.
+          </p>
+        </div>
         <div className=" flex flex-col sm:grid sm:grid-cols-2 justify-evenly items-center gap-10">
-      
           <div className="">
             <h6 class="font-bold text-xl black py-1 text-primary">
-              Ticket sales
-            </h6>
+            24/7 Chat Support            </h6>
             <p className="sm:mt-4">
-              From online conversations to concerts, creators are always looking
-              for opportunities to interact with their fans. With ticket sales,
-              fans can connect with creators and pay them for their time and
-              energy.
+            Meet your customers' needs at any hour. Our round-the-clock chat support ensures you never miss a beat, and with instant responses, customer satisfaction is just a few clicks away.
             </p>{" "}
-            <div className=" sm:w-[80%] sm:mt-5">
+            <div className=" sm:w-[85%] sm:mt-5">
               <div className="w-full  mt-[40px] sm:mt-0">
                 <div className="h-[100%]  mb-6 sm:mb-0">
                   <img
                     src="/ticketed_sales.webp"
-                    className="w-full sm:h-[354px] m-auto "
+                    className="w-full sm:h-[354px] m-auto shadow-md"
                   />
                 </div>
               </div>
@@ -45,19 +37,16 @@ const Tempoeconomy = () => {
           <div className="">
             {" "}
             <h6 class="font-bold text-xl black py-1 text-primary">
-              Tips from fans
-            </h6>
+            Intelligent Automation            </h6>
             <p className="sm:mt-4">
-              Drive increased creator-to-fan engagement by unlocking tipping for
-              content on your platform. Show your support for creators by
-              tipping them for the content they create.
+            Leverage the power of advanced AI models, ChatGPT and GPT-4. Intelligent automation not only streamlines your operations but also keeps improving as it learns from your data.
             </p>
-            <div className=" sm:w-[80%] sm:mt-5">
+            <div className=" sm:w-[85%] sm:mt-5">
               <div className="w-full  mt-[40px] sm:mt-0">
                 <div className="h-[100%]  mb-6 sm:mb-0">
                   <img
                     src="/tips_from_fans.webp"
-                    className="w-full sm:h-[354px] m-auto "
+                    className="w-full sm:h-[354px] m-auto shadow-md"
                   />
                 </div>
               </div>
@@ -65,19 +54,16 @@ const Tempoeconomy = () => {
           </div>
           <div className="">
             <h6 class="font-bold text-xl black py-1 text-primary">
-              Content and membership subscriptions{" "}
-            </h6>
+            Seamless Integrations            </h6>
             <p className="sm:mt-4">
-              Allow creators to build recurring revenue streams with their fans
-              through memberships and subscriptions. Creators can sell written
-              content, podcasts, online courses, and more.
+            Enjoy the ease of plug-and-play with Tempo Chat. Integrate your payments, CRM, inventory management, and backend with a single click.
             </p>{" "}
-            <div className=" sm:w-[80%] sm:mt-5">
+            <div className=" sm:w-[85%] sm:mt-5">
               <div className="w-full  mt-[40px] sm:mt-0">
                 <div className="h-[100%]  mb-6 sm:mb-0">
                   <img
                     src="/membership_subscriptions.webp"
-                    className="w-full sm:h-[354px] m-auto "
+                    className="w-full sm:h-[354px] m-auto shadow-md "
                   />
                 </div>
               </div>
@@ -86,19 +72,16 @@ const Tempoeconomy = () => {
           <div className="">
             {" "}
             <h6 class="font-bold text-xl black py-1 text-primary">
-              Financial services
-            </h6>
+            Customized Policies            </h6>
             <p className="sm:mt-4">
-              Use our Banking-as-a-Service APIs—Treasury and Issuing—to offer
-              money-management accounts and issue cards with custom rewards to
-              the creators on your platform.
+            Ensure the AI aligns with your brand. Train Tempo Chat on your policies using our LLM connectors, customize your workflows, and deliver a consistent customer experience.
             </p>
-            <div className=" sm:w-[80%] sm:mt-5">
+            <div className=" sm:w-[85%] sm:mt-5">
               <div className="w-full  mt-[40px] sm:mt-0">
                 <div className="h-[100%]  mb-6 sm:mb-0">
                   <img
                     src="/digital_wallet.webp"
-                    className="w-full sm:h-[354px] m-auto "
+                    className="w-full sm:h-[354px] m-auto  shadow-md"
                   />
                 </div>
               </div>
