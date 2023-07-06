9b33188227a82df02e37ce7275c667cfeff9713a
joywithyashh <yash@usetempo.ai>
2023-07-07 03:15:53 +0530

    blogs

--- app/components/Blog/BlogSidebar.js
+++ app/components/Blog/BlogSidebar.js
@@ -86,11 +86,7 @@ const BlogSidebar = ({ children }) => {
     <>
       <div className="block sm:flex md:flex lg:flex justify-start mt-5">
         <div className="hidden sm:block md:block lg:block w-[20%]">
-          <div class="mb-3   top-0  border-border rounded-sm border-t-0 border-l-0 min-h-screen ">
-            <div className="fixed top-30">
-              <p className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white ">
-                Latest Articles
-              </p>
+        
               {/* <div className=" ml-4  font-medium SideOptions">
                 {upperTitle.map((ele, key) => (
                   <div
@@ -158,61 +154,7 @@ const BlogSidebar = ({ children }) => {
                   </div>
                 ))}
               </div> */}
-              <div class="relative  flex w-[240px] bg-[#eedede] flex-wrap items-stretch p-7">
-                <input
-                  type="search"
-                  className="relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
-                  placeholder="Search"
-                  aria-label="Search"
-                  aria-describedby="button-addon1"
-                />
-                <button
-                  className="relative z-[2] flex items-center rounded-r bg-[#142543] px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
-                  type="button"
-                  id="button-addon1"
-                  data-te-ripple-init
-                  data-te-ripple-color="light"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    class="h-5 w-5"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="w-full px-10 sm-w-[80%] md-w-[80%] lg-w-[80%] ">
-          <div className="block sm:hidden md:hidden lg:hidden">
-            <Accordion allowZeroExpanded>
-              <AccordionItem>
-                <AccordionItemHeading>
-                  <AccordionItemButton>
-                    <h5 className="text-heading text-md font-semibold">
-                      {" "}
-                      Latest Articles
-                    </h5>
-                  </AccordionItemButton>
-                </AccordionItemHeading>
-                {upperTitle.map((ele, key) => (
-                  <div className="group " key={key}>
-                    <AccordionItemPanel>{ele.name}</AccordionItemPanel>
-                  </div>
-                ))} 
-              </AccordionItem>
-            </Accordion>
-          </div>
-          <hr class="h-px my-8 bg-gray-200 border-b-0 dark:bg-gray-700 left-0" />
-          {children}
-          <Footer />
+             
         </div>
       </div>
     </>
