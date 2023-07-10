8727d5df85b9b59feb3d2b99d894f0974c344d31
joywithyashh <yash@usetempo.ai>
2023-07-10 22:28:06 +0530

    changes

--- app/components/Checkout/CheckOutForm.js
+++ app/components/Checkout/CheckOutForm.js
@@ -1,35 +1,68 @@
 "use-client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   CardElement,
   Elements,
+  PaymentElement,
+  LinkAuthenticationElement,
   useElements,
   useStripe,
 } from "@stripe/react-stripe-js";
-import { submitCheckout } from "@/app/API/pages/Checkout";
+import { createPaymentIntent, submitCheckout } from "@/app/API/pages/Checkout";
 import { useRouter } from "next/navigation";
 import { subscribeCustomer } from "@/app/API/pages/Checkout";
 import Button from "../Common/Button/Button";
 import { createNewGoogleUser } from "@/app/API/pages/Login";
 
 
-const CheckOutForm = ({ checkoutForm, boxValid, googleAuthInfo }) => {
+const CheckOutForm = ({ checkoutForm, boxValid, googleAuthInfo, client_secret, }) => {
   const router = useRouter();
   const stripe = useStripe();
   const elements = useElements();
+  const [message, setMessage] = useState(null)
 
   const [errors, setError] = useState([]);
   const [loading, setLoading] = useState();
+  const paymentElementOptions = {
+    layout: "tabs"
+  }
 
+  const getPaymentIntent = async () => {
+    if (!stripe) {
+      return;
+    }
 
+    // if (!clientSecret?.client_secret) {
+    //   return;
+    // }
 
+    stripe.retrievePaymentIntent(client_secret).then(({ paymentIntent }) => {
+      switch (paymentIntent.status) {
+        case "succeeded":
+          setMessage("Payment succeeded!");
+          break;
+        case "processing":
+          setMessage("Your payment is processing.");
+          break;
+        case "requires_payment_method":
+          setMessage("Your payment was not successful, please try again.");
+          break;
+        default:
+          setMessage("Something went wrong.");
+          break;
+      }
+    });
+  }
+  useEffect(() => {
+    getPaymentIntent()
+  }, [stripe]);
   const handleCheckout = async (e) => {
     e.preventDefault();
     setLoading(true);
     try {
       const { error, paymentMethod } = await stripe.createPaymentMethod({
         type: "card",
-        card: elements.getElement(CardElement),
+        card: elements.getElement(PaymentElement),
       });
       if (error) {
         console.log(error)
@@ -51,7 +84,7 @@ const CheckOutForm = ({ checkoutForm, boxValid, googleAuthInfo }) => {
       }
 
       const result = googleAuthInfo.googleLogin ? await createNewGoogleUser(googleAuthInfoPayload) : await submitCheckout(checkoutForm2)
-      
+
       if (result.token) {
         let bodyForSubscribe = {
           token: paymentMethod.id,
@@ -84,14 +117,42 @@ const CheckOutForm = ({ checkoutForm, boxValid, googleAuthInfo }) => {
     return messages;
   }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js hasn't yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+
+    setLoading(true);
+
+    const { error } = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        // Make sure to change this to your payment completion page
+        return_url: "http://localhost:3000",
+      },
+    });
+
+    if (error.type === "card_error" || error.type === "validation_error") {
+      setMessage(error.message);
+    } else {
+      setMessage("An unexpected error occurred.");
+    }
+
+    setLoading(false);
+  };
   return (
     <>
-      <form onSubmit={handleCheckout}>
+      <form onSubmit={handleSubmit}>
+        <PaymentElement id="payment-element" options={paymentElementOptions} />
         <div
           className="border rounded px-2 border-gray-100"
           style={{ borderColor: "#80808080" }}
         >
-          <CardElement
+          {/* <CardElement
             className="form-control"
             options={{
               style: {
@@ -105,12 +166,12 @@ const CheckOutForm = ({ checkoutForm, boxValid, googleAuthInfo }) => {
                 },
               },
             }}
-          />
+          /> */}
         </div>
 
         {loading && <p className="message">Processing Payment...</p>}
         <Button type={"submit"} className="flex w-full mx-auto mt-4 justify-center px-4 py-2 text-white hover:border hover:bg-white hover:text-black bg-black border border-gray-300 rounded-md shadow-sm checkout"
-          disabled={boxValid}
+          disabled={loading || !stripe || !elements}
         >
           Checkout
         </Button>
--- app/components/Stripe/Wrapper/StripeWrapper.js
+++ app/components/Stripe/Wrapper/StripeWrapper.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 const STRIPE_KEY = process.env.NEXT_PUBLIC_STRIPE_KEY;
-const StripeWrapper = ({ children }) => {
+const StripeWrapper = ({ children, options = null }) => {
     const stripeLib = loadStripe(STRIPE_KEY);
     return (
-        <Elements stripe={stripeLib}>{children}</Elements>
+        <Elements options={options} stripe={stripeLib}>{children}</Elements>
     )
 }
 export default StripeWrapper
