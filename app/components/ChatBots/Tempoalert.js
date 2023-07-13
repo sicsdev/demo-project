7104e5305ba18a46db16a306906adefb9d7f24cb
vivekTempo <vivek@useTempo.ai>
2023-07-14 04:49:55 +0530

    add new

--- app/components/ChatBots/Tempoalert.js
+++ app/components/ChatBots/Tempoalert.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 import { useState } from "react";
 import Card from "../Common/Card/Card";
+import Button from "../Common/Button/Button";
 
 const Tempoalert = () => {
   const [hide, setHide] = useState({
@@ -9,17 +10,31 @@ const Tempoalert = () => {
   });
   return (
     <div className="bg-background py-6 sm:py-5">
-     <div className="max-w-[1400px] w-full m-auto sm:py-8 md:py-8 lg:py-8 px-0 sm:px-4 lg:px-4 relative group">
-        <h2 className="font-bold  text-2xl  md:text-h2 lg:text-h2 sm:text-h2 text-center  sm:my-8  leading-[40px] sm:leading-[60px] text-white">
-        Stay Ahead with Round-the-Clock Support and Seamless Integration        </h2>
-        <div className="block sm:flex md:flex lg:flex justify-evenly gap-10">
-          <div className="p-4 sm:p-[3rem]">
+      <div className="max-w-[1400px] w-full m-auto sm:py-8 md:py-8 lg:py-8 px-0 sm:px-4 lg:px-4 relative group">
+        <div className="flex flex-col sm:grid sm:grid-cols-2">
+          <div>
+            <h2 className="font-bold  text-[25px]   px-6 sm:px-0 sm:text-[25px] text-center  sm:my-8 sm:text-start leading-[35px] sm:ml-[75px] sm:leading-[44px] text-white">
+              Stay Ahead with Round-the-Clock Support and Seamless Integration{" "}
+            </h2>
+          </div>
+
+          <div>
+            <Button
+              className="flex  sm:w-full mx-auto max-w-[70%] sm:max-w-[50%] mt-4 sm:mt-10 justify-center px-4 py-2 text-white hover:border hover:bg-white hover:text-black bg- border border-gray-300 rounded-md shadow-sm checkout"
+            >
+              Learn more about security{" "}
+            </Button>
+          </div>
+        </div>
+
+        <div className="block sm:grid sm:grid-cols-2 justify-evenly ">
+          <div className="p-4">
             <div className="flex justify-start gap-6">
               <div className="relative w-[90px] h-[35px]">
-              <span className="flex sm:gap-3  text-custom-small  sm:mt-1 sm:text-3xl align-bottom font-semibold">
-                      {" "}
-                      &#x2713; 
-                    </span>
+                <span className="flex sm:gap-3  text-custom-small  sm:mt-1 sm:text-3xl align-bottom font-semibold">
+                  {" "}
+                  &#x2713;
+                </span>
               </div>
               <div>
                 <h3 className="font-bold  text-[18px] md:text-h5 lg:text-h5 sm:text-h5    text-white">
@@ -28,17 +43,19 @@ const Tempoalert = () => {
                 <p className="text-white">
                   Stay ahead of the game with Tempo Chat. No need to hire around
                   the clock and overpay while missing your SLAs with subpar
-                  quality. Tempo Chat is always there when your customers
-                  need support.
+                  quality. Tempo Chat is always there when your customers need
+                  support.
                 </p>
               </div>
             </div>
-            <div className="flex justify-start gap-6 mt-6">
+          </div>
+          <div className="p-4 ">
+            <div className="flex justify-start gap-6 ">
               <div className="relative w-[90px] h-[35px]">
-              <span className="flex  sm:gap-3  text-custom-small  sm:mt-1 sm:text-3xl  align-bottom font-semibold">
-                      {" "}
-                      &#x2713; 
-                    </span>
+                <span className="flex  sm:gap-3  text-custom-small  sm:mt-1 sm:text-3xl  align-bottom font-semibold">
+                  {" "}
+                  &#x2713;
+                </span>
               </div>
               <div>
                 <h3 className="font-bold text-[18px]  md:text-h5 lg:text-h5 sm:text-h5 text-white">
@@ -51,68 +68,60 @@ const Tempoalert = () => {
                 </p>
               </div>
             </div>
-            <div className="flex justify-start gap-6 mt-6">
-            <div className="relative w-[90px] h-[35px]">
-            <span className="flex  sm:gap-3  text-custom-small  sm:mt-1 sm:text-3xl  align-bottom font-semibold">
-                      {" "}
-                      &#x2713; 
-                    </span>
-            </div>
-            <div>
-              <h3 className="font-bold text-[18px]  md:text-h5 lg:text-h5 sm:text-h5 text-white">
-                Clear and Fair Billing: Only Pay for What You Use{" "}
-              </h3>
-              <p className="text-white relative"
-                          onMouseLeave={() =>
-                            setHide({ first: false })}>
-                At Tempo Chat, transparency is our mantra. Our straightforward
-                billing structure charges only 25 cents per chat response. Enjoy
-                the simplicity of paying only for what you truly use.{" "}
-
-              <span
-                className="cursor-pointer"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  setHide({ first: true });
-                }}
-              >
-                *
-              </span>
-              {hide.first == true ? (
-                <Card
-                  className={
-                    "animate-fadeIn w-[320px] sm:w-[400px] absolute z-50 top-[30px] bg-white ml-auto mr-auto left-0 right-0"
-                  }
+          </div>
+          <div className="p-4 ">
+            <div className="flex justify-start gap-6 ">
+              <div className="relative w-[90px] h-[35px]">
+                <span className="flex  sm:gap-3  text-custom-small  sm:mt-1 sm:text-3xl  align-bottom font-semibold">
+                  {" "}
+                  &#x2713;
+                </span>
+              </div>
+              <div>
+                <h3 className="font-bold text-[18px]  md:text-h5 lg:text-h5 sm:text-h5 text-white">
+                  Clear and Fair Billing: Only Pay for What You Use{" "}
+                </h3>
+                <p
+                  className="text-white relative"
+                  onMouseLeave={() => setHide({ first: false })}
                 >
-                  <p
-                    className="text-heading"
-                    onMouseLeave={() => setHide({ first: false })}
+                  At Tempo Chat, transparency is our mantra. Our straightforward
+                  billing structure charges only 25 cents per chat response.
+                  Enjoy the simplicity of paying only for what you truly use.{" "}
+                  <span
+                    className="cursor-pointer"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      setHide({ first: true });
+                    }}
                   >
-                    Usage-based pricing includes customer-initiated interactions. If you would like to limit the number of interactions, you can set a hard spend limit within your profile.
-                  </p>
-                </Card>
-              ) : (
-                ""
-              )}
-            </p>
-
-           
+                    *
+                  </span>
+                  {hide.first == true ? (
+                    <Card
+                      className={
+                        "animate-fadeIn w-[320px] sm:w-[400px] absolute z-50 top-[30px] bg-white ml-auto mr-auto left-0 right-0"
+                      }
+                    >
+                      <p
+                        className="text-heading"
+                        onMouseLeave={() => setHide({ first: false })}
+                      >
+                        Usage-based pricing includes customer-initiated
+                        interactions. If you would like to limit the number of
+                        interactions, you can set a hard spend limit within your
+                        profile.
+                      </p>
+                    </Card>
+                  ) : (
+                    ""
+                  )}
+                </p>
+              </div>
             </div>
           </div>
-          </div>
-          <div className="text-center">
-            <div className="relative h-100 sm:h-[363px] w-100 sm:w-[543px]">
-              <Image
-                src="/middle.png"
-                className="bg-contains w-full"
-                fill={true}
-              />
-            </div>
-          </div>
-      
         </div>
       </div>
-
     </div>
   );
 };
