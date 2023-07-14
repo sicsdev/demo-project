b39d79d687fc043e8bae73681a8b8f2a35daf043
Gaurav Sharma <gaurav@usetempo.ai>
2023-07-15 05:04:03 +0530

    Changes in Integration and Learning Center

--- app/(dashboard)/dashboard/layout.js
+++ app/(dashboard)/dashboard/layout.js
@@ -1,15 +1,12 @@
 
 import Dashboard from "@/app/components/DashboardSubLayout/Dashboard";
-import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
 
 export default function DashboardLayout({ children }) {
   return (
     <>
       <Dashboard>
         {children}
-        <ToastContainer />
       </Dashboard>
     </>
   );
