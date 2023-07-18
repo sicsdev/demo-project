77146a624c02a6f735455e4d7afeff3aac40b79d
vivekTempo <vivek@useTempo.ai>
2023-07-18 16:38:50 +0530

    cards accordion

--- app/components/featurescards/page.js
+++ app/components/featurescards/page.js
@@ -1,8 +1,6 @@
-"use client";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import {
   Accordion,
   AccordionItem,
