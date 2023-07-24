835718af5a84044f88ef2194f949aa2330059c0d
vivekTempo <vivek@useTempo.ai>
2023-07-25 04:55:12 +0530

    add marketing component

--- app/(pages)/page.js
+++ app/(pages)/page.js
@@ -31,7 +31,7 @@ export default function Home() {
       <Newstandard />
       {/* <Trial /> */}
       <Social />
-      {/* <Marketing/> */}
+      <Marketing/>
 
       <Launch />
       {/* <Benifits />         */}
