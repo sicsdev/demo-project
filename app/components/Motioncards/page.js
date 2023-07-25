2f853a4c41a9a12437a587aba70029579bc931f7
vivekTempo <vivek@useTempo.ai>
2023-07-25 22:27:30 +0530

    changes

--- app/components/Motioncards/page.js
+++ app/components/Motioncards/page.js
@@ -6,25 +6,25 @@ const cards = [
     id: "1",
     img: "/integrations/github.svg",
     name: "Github",
-    subheading: "File Management",
+    subheading: "Productivity",
   },
   {
     id: "2",
     img: "/twilio-logo-png-transparent.png",
     name: "Twilio",
-    subheading: "Communication",
+    subheading: "Tools",
   },
   {
     id: "3",
-    img: "/marque/3.svg",
-    name: "Mero",
-    subheading: "Collaboration",
+    img: "/integrations/square.svg",
+    name: "Square",
+    subheading: "Billing",
   },
   {
     id: "4",
-    img: "/marque/4.svg",
-    name: "Email",
-    subheading: "Communication",
+    img: "/brain.png",
+    name: "Braintree",
+    subheading: "Braintree",
   },
   {
     id: "5",
@@ -32,8 +32,125 @@ const cards = [
     name: "Slack",
     subheading: "Communication",
   },
+  {
+    id: "6",
+    img: "/paypal-icon-logo-png-transparent.png",
+    name: "Paypal",
+    subheading: "Billing",
+  },
+  {
+    id: "7",
+    img: "/stripe_logo_icon_167962.png",
+    name: "Stripe",
+    subheading: "Billing",
+  },
+  {
+    id: "7",
+    img: "/integrations/freshdesk.svg",
+    name: "FreshDesk",
+    subheading: "Helpdesk",
+  },
 ];
-
+const card1 = [
+  {
+    id: "1",
+    img: "/teams.png",
+    name: "Microsoft Teams",
+    subheading: "Communication",
+  },
+  {
+    id: "2",
+    img: "/integrations/front.svg",
+    name: "Front",
+    subheading: "Helpdesk",
+  },
+  {
+    id: "3",
+    img: "/zen.png",
+    name: "ZenDesk",
+    subheading: "Helpdesk",
+  },
+  {
+    id: "4",
+    img: "/geo.png",
+    name: "Gorgias",
+    subheading: "Helpdesk",
+  },
+  {
+    id: "5",
+    img: "/integrations/intercom.svg",
+    name: "Intercom",
+    subheading: "Helpdesk",
+  },
+  {
+    id: "6",
+    img: "/58482ec0cef1014c0b5e4a70.png",
+    name: "Shopify",
+    subheading: "Billing",
+  },
+  {
+    id: "7",
+    img: "/salesforce-logo.png",
+    name: "SalesForce",
+    subheading: "Sales",
+  },
+  {
+    id: "8",
+    img: "/HUBS-3bd277ce.png",
+    name: "HubSpot",
+    subheading: "Sales",
+  },
+];
+const card2 = [
+  {
+    id: "1",
+    img: "/asana-logo-logo.png",
+    name: "Asana",
+    subheading: "Productivity",
+  },
+  {
+    id: "2",
+    img: "/monday_logo_icon_168967_1.png",
+    name: "Monday",
+    subheading: "Productivity",
+  },
+  {
+    id: "3",
+    img: "/Jira.png",
+    name: "Jira",
+    subheading: "Productivity",
+  },
+  {
+    id: "4",
+    img: "/linear-logos-id-IUp6ZRH.png",
+    name: "Linear",
+    subheading: "Productivity",
+  },
+  {
+    id: "5",
+    img: "/pngwing.png",
+    name: "CircleCI",
+    subheading: "Tools",
+  },
+  {
+    id: "6",
+    img: "/datadog-logo-purple.png",
+    name: "DataDog",
+    subheading: "Tools",
+  },
+  {
+    id: "7",
+    img: "/SendGrid-Logomark.png",
+    name: "SendGrid",
+    subheading: "Tools",
+  },
+  {
+    id: "8",
+    img: "/menu_logo.png",
+    name: "NMI",
+    subheading: "Billing",
+  },
+];
 const Motioncards = () => {
   return (
     <div className="bg-white motion_marque_section  py-3 m-auto">
@@ -46,9 +163,10 @@ const Motioncards = () => {
             Integrate your most important API's and databases to empower Tempo
             to control your CS experience.
           </p>
-          <button className="py-2 px-8 sm:w-[60%] w-[100%] sm:px-10 mt-4 md:px-10 lg:px-5 sm:py-5 md:py-5 lg:py-3 text-white text-lg font-semibold bg-primary hover:bg-black dark:focus:ring-yellow-900 rounded-lg">
-              {" "}
-              <Link href="/pricing"> Start Now </Link>
+       
+          
+<button className="text-primary my-3 sm:my-0 text-lg font-semibold dark:focus:ring-yellow-900 rounded-lg">
+<Link href="/pricing"> Start Now →</Link>
           </button>
         </div>
         {/* marque */}
@@ -77,7 +195,7 @@ const Motioncards = () => {
           </div>
           <div class="flex overflow-hidden wrapper_marque">
             <Marquee pauseOnHover={true} loop={0} direction="left">
-              {cards.map((item) => (
+              {card1.map((item) => (
                 <Link
                   key={item.id}
                   className="marque_card inline-block m-3"
@@ -99,7 +217,7 @@ const Motioncards = () => {
           </div>
           <div class="flex overflow-hidden wrapper_marque">
             <Marquee pauseOnHover={true} loop={0} direction="right">
-              {cards.map((item) => (
+              {card2.map((item) => (
                 <Link
                   key={item.id}
                   className="marque_card inline-block m-3"
