755767fad0d2a2d29737d81336207678819ac55e
vivekTempo <vivek@useTempo.ai>
2023-07-27 02:14:30 +0530

    add middlebar

---
+++ app/components/Info-Screen/Middlebar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "next/link";
+const Middlebar = () => {
+  return (
+<div class=" md:block">
+    <div className="z-40 text-center p-3 bg-[#142543]">
+
+      <h6 className="text-blue-400 text-white xs:flex-row xs:flex-col sm:flex justify-center text-[12px] md:text-para lg:text-para sm:text-para sm:leading-8 gap-2">
+      Reduce your costs today with Tempo's no-code GPT-powered customer service technology!        {/* Onboard real agents powered by ChatGPT to plug into Shopify, Salesforce,
+        Zendesk, or your custom store. */}
+        {" "}
+        {/* <span className="underline cursor-pointer "> Start Now</span> */}
+    
+      </h6>
+      </div>
+    </div>
+  );
+};
+
+export default Middlebar;
