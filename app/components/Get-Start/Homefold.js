195d1206cd3f2fe9e27e33e548eaff5e8905cb04
vivekTempo <vivek@useTempo.ai>
2023-07-28 04:36:37 +0530

    change line

--- app/components/Get-Start/Homefold.js
+++ app/components/Get-Start/Homefold.js
@@ -15,12 +15,12 @@ const Homefold = () => {
     <>
       <div className="bg-home-above relative h-[700px]">
         <div className="absolute w-[35%] h-full flex right-0">
-          <img src="/home-fold.png" className="object-fill w-full" />
+          <img src="/home-fold.png" className="object-fill w-full md:opacity-100	opacity-[0.75]" />
         </div>
         <div className="absolute w-[35%] h-full flex left-0">
-        <img src="/left-side.png" className="object-fill w-full" />
+        <img src="/left-side.png" className="object-fill w-full md:opacity-100	opacity-[0.75]" />
       </div>
-        <div className=" mx-auto max-w-[90%] sm:max-w-[80%] md:max-w-[80%] lg:max-w-[80%]  py-20 relative">
+        <div className=" mx-auto max-w-[90%] sm:max-w-[80%] md:max-w-[80%] lg:max-w-[80%]  py-12 sm:py-20 relative">
           <div className="block sm:flex   justify-center  gap-10">
             <div>
               {loading ? (
@@ -30,13 +30,13 @@ const Homefold = () => {
               ) : (
                 <p
                   className={
-                    "text-center   font-bold  w-[40%] sm:w-[136px] m-auto text-base py-1 sm:mt-8  px-1  focus:ring-yellow-300 text-white rounded-[20px] bg-primary hover:bg-black dark:focus:ring-yellow-900"
+                    "text-center   font-bold mt-[60px]   w-[40%] sm:w-[136px] m-auto text-base py-1 sm:mt-8  px-1  focus:ring-yellow-300 text-white rounded-[20px] bg-primary hover:bg-black dark:focus:ring-yellow-900"
                   }
                 >
                   INTRODUCING{" "}
                 </p>
               )}
-              <div className="relative flex items-center gap-4 w-[60%] mx-auto">
+              <div className="relative flex items-center gap-4 w-full my-6 sm:my-0 sm:w-[60%] mx-auto">
                 <div
                   data-line-position="left"
                   class="connect-heading-line  sm:mt-8 mt-4"
@@ -49,7 +49,7 @@ const Homefold = () => {
                    <div className="block sm:hidden"><SkeletonLoader height={80} width={100}  baseColor="#230A5C"/></div>
                     </>
                   ) : (
-                    <img src="/logo.png" className="w-[30rem] sm:w-[65rem]" />
+                    <img src="/logo.png" className="w-[50rem] sm:w-[65rem]" />
                   )}
                 </h1>
 
@@ -59,7 +59,7 @@ const Homefold = () => {
                 ></div>
               </div>
 
-              <div className="font-bold text-2xl m-auto text-center mt-4  md:text-h3 lg:text-h3 sm:text-h3 t  sm:mt-8 mb-2 sm:mb-4 text-white">
+              <div className="font-bold text-2xl m-auto w-[300px] sm:w-full text-center mt-4  md:text-h3 lg:text-h3 sm:text-h3 t  sm:mt-8 mb-2 sm:mb-4 text-white">
                 {loading ? (
                   <>
                     <div className="hidden sm:block">
@@ -78,7 +78,7 @@ const Homefold = () => {
                   <SkeletonLoader count={2} height={30} width={"100%"}  baseColor="#230A5C"/>
                 </p>
               ) : (
-                <p className=" text-xl text-center  md:text-xl sm:max-w-[632px] sm:ml-[12rem] sm:mt-8   mt-4 mb-2 sm:mb-4 text-white">
+                <p className=" text-xl text-center  md:text-xl sm:max-w-[632px] sm:ml-[12rem] sm:mt-8   mt-[1.5rem] mb-[1.5rem] sm:mb-4 text-white">
                   Watch your costs plummet with AI-powered customer service.
                   50%+ deflection, instant SLAs, and 24/7 service.
                 </p>
@@ -89,7 +89,7 @@ const Homefold = () => {
                 ) : (
                   <button
                     className={
-                      "py-2 px-8 w-full first-letter:w-full focus:ring-yellow-300    text-lg font-semibold text-white bg-[#fe9327] hover:bg-black dark:focus:ring-yellow-900 rounded-lg"
+                      "py-2 px-8 mb-[12px] sm:mb-0 w-full first-letter:w-full focus:ring-yellow-300    text-lg font-semibold text-white bg-[#fe9327] hover:bg-black dark:focus:ring-yellow-900 rounded-lg"
                     }
                   >
                     <div className="trytempo">
@@ -103,7 +103,7 @@ const Homefold = () => {
                   <button
                     type={"submit"}
                     className={
-                      "py-2 px-8 focus:ring-yellow-300 text-white w-full hover:bg-white hover:text-primary  mt-2 sm:m-0 md:m-0 lg:m-0 text-lg font-semibold border dark:focus:ring-yellow-900 rounded-lg"
+                      "py-2 px-8 mb-[10px] sm-mb-0 focus:ring-yellow-300 text-white w-full hover:bg-white hover:text-primary  mt-2 sm:m-0 md:m-0 lg:m-0 text-lg font-semibold border dark:focus:ring-yellow-900 rounded-lg"
                     }
                   >
                     <div
