99924c90835d62ea5bbd0b1fed4ecfd1d3785416
joywithyashh <yash@usetempo.ai>
2023-08-05 03:10:58 +0530

    articles

--- app/(pages)/article/linear-integration/page.js
+++ app/(pages)/article/linear-integration/page.js
@@ -67,6 +67,7 @@ const page = () => {
             )}
           </p>
         )}
+        
         {loading ? (
           <h1 className="font-bold text-center px-4 pt-8 text-3xl text-heading  md:text-h2 lg:text-h3 sm:text-h2 sm:leading-none">
             <SkeletonLoader className="sm:h-[70px] sm:w-[800px]" />
