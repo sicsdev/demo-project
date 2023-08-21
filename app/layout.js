19a9d4bc740a31d50afff3ff7cbfc9e44ba8e226
joywithyashh <yash@usetempo.ai>
2023-08-22 04:29:07 +0530

    hotjar

--- app/layout.js
+++ app/layout.js
@@ -76,11 +76,11 @@ export default function RootLayout({ children }) {
 
         
         {/* Google tag */}
-        {/* <script
+        <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-HFHNKD99J4"
-        ></script> */}
-        {/* <script
+        ></script>
+        <script
           dangerouslySetInnerHTML={{
             __html: `
         window.dataLayer = window.dataLayer || [];
@@ -91,7 +91,7 @@ export default function RootLayout({ children }) {
       `,
           }}
 
-        /> */}
+        />
         {/* <!-- Google Tag Manager -->  */}
 
         
