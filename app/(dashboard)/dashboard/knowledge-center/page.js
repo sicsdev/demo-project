57b3d6e54ab04f47a02373b746d85ba4462b9485
joywithyashh <yash@usetempo.ai>
2023-09-20 01:29:54 +0530

    changes

--- app/(dashboard)/dashboard/knowledge-center/page.js
+++ app/(dashboard)/dashboard/knowledge-center/page.js
@@ -382,49 +382,51 @@ const Page = () => {
                         </div>
                     </div>
                 ) : ( */}
-                    <>
-                        <div className="w-full" >
+                <>
+                    <div className="w-full" >
 
 
-                            <div className='flex justify-end gap-4 items-center mt-2 p-2'>
-                                <label htmlFor="search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                                <div className="relative">
-                                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                        <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                                        </svg>
-                                    </div>
-                                    <input type="search" id="search" className="border border-input_color w-full block  px-2 py-2 bg-white focus:bg-white  rounded-md shadow-sm placeholder-slate-400  focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50  invalid:border-pink-500  focus:invalid:border-pink-500 focus:invalid:ring-pink-500 pl-10" placeholder="Search" value={search} onChange={(e) => { handleChange(e) }} />
+                        <div className='flex justify-end gap-4 items-center mt-2 p-2'>
+                            <label htmlFor="search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                            <div className="relative">
+                                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                    </svg>
                                 </div>
+                                <input type="search" id="search" className="border border-input_color w-full block  px-2 py-2 bg-white focus:bg-white  rounded-md shadow-sm placeholder-slate-400  focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50  invalid:border-pink-500  focus:invalid:border-pink-500 focus:invalid:ring-pink-500 pl-10" placeholder="Search" value={search} onChange={(e) => { handleChange(e) }} />
                             </div>
-
-
-                            <DataTable
-                                title={''}
-                                fixedHeader
-                                highlightOnHover
-                                pointerOnHover
-                                defaultSortFieldId="number_of_messages"
-                                pagination
-                                columns={columns}
-                                noDataComponent={<><p className="text-center text-xs p-3">Questions Tempo needs your help answering will show here when they're ready!</p></>}
-                                data={state?.data?.results.filter(item => item.accepted === false)}
-                                progressPending={loading}
-                                progressComponent={<div className="w-full mt-3 relative"><SkeletonLoader count={9} height={30} width="100%" className={"mt-2"} /></div>}
-                                paginationDefaultPage={pageVal}
-                                paginationPerPage={perPage}
-                                paginationTotalRows={state?.data?.count}
-                                paginationServer
-                                onChangeRowsPerPage={handlePerRowsChange}
-                                onChangePage={(page) => {
-                                    handleRecomodationValue(page)
-                                }}
-                                sortServer
-                                onSort={handleSort}
-                                customStyles={customStyles}
-                            />
                         </div>
-                    </>
+
+
+                        <DataTable
+                            title={''}
+                            fixedHeader
+                            highlightOnHover
+                            pointerOnHover
+                            defaultSortFieldId="number_of_messages"
+                            pagination
+                            columns={columns}
+                            noDataComponent={<><p className="text-center text-xs p-3">Questions Tempo needs your help answering will show here when they're ready!</p></>}
+                            data={state?.data?.results.filter(item => item.accepted === false)}
+                            progressPending={loading}
+                            progressComponent={<div className="w-full mt-3 relative"><SkeletonLoader count={9} height={30} width="100%" className={"mt-2"} /></div>}
+                            paginationDefaultPage={pageVal}
+                            paginationPerPage={perPage}
+                            paginationTotalRows={state?.data?.count}
+                            paginationServer
+                            onChangeRowsPerPage={handlePerRowsChange}
+                            onChangePage={(page) => {
+                                handleRecomodationValue(page)
+                            }}
+
+                            className=''
+                            sortServer
+                            onSort={handleSort}
+                            customStyles={customStyles}
+                        />
+                    </div>
+                </>
                 {/* )} */}
             </div>
             <ToastContainer />
