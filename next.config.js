ac55ebcbb538e70e95792d3cc1a605a0fe1e67f3
zkcastTMP <134722001+zkcastTMP@users.noreply.github.com>
2023-09-21 23:36:41 +0200

    Add: Sentry.

--- next.config.js
+++ next.config.js
@@ -1,37 +1,43 @@
-/** @type {import('next').NextConfig} */
+const { withSentryConfig } = require("@sentry/nextjs");
+
+
 const nextConfig = {
-  
-    images: {
-      domains: [
-        "dam.freshworks.com",
-        "www.freshworks.com",
-        "assets-global.website-files.com",
-        "static.intercomassets.com",
-        "res.cloudinary.com",
-        "randomuser.me",
-        "avatars.slack-edge.com",
-      ],
-      unoptimized: true,
-    },
-    reactStrictMode: true,
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    output: 'export',
-    reactStrictMode: false,
-    // async rewrites() {
-    //   return [
-    //     {
-    //       source: "/robots.txt",
-    //       destination: "/api/robots",
-    //     },
-    //   ];
-    // },
-
-    // To use Amazon s3 we need to use output export, but this is not compatible with rewrites.
-  };
-  
-  module.exports = nextConfig;
+  images: {
+    domains: [
+      "dam.freshworks.com",
+      "www.freshworks.com",
+      "assets-global.website-files.com",
+      "static.intercomassets.com",
+      "res.cloudinary.com",
+      "randomuser.me",
+      "avatars.slack-edge.com",
+    ],
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  output: 'export',
+  reactStrictMode: false,
+  sentry: {
+    disableServerWebpackPlugin: true,
+    disableClientWebpackPlugin: true,
+  },
+}
 
 
-  
+module.exports = withSentryConfig(
+  nextConfig,
+  {
+    //Sentry config
+    silent: true,
+    org: "tempo-ee56a7c44",
+    project: "tempo-ai",
+    widenClientFileUpload: true,
+    transpileClientSDK: true,
+    tunnelRoute: "/monitoring",
+    hideSourceMaps: true,
+    disableLogger: true,
+  }
+);
