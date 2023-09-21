2439cd6182f292f1d99c2be072614dcc7a82e472
joywithyashh <yash@usetempo.ai>
2023-09-21 22:29:16 +0530

    knowledge change

--- app/(dashboard)/dashboard/knowledge-center/page.js
+++ app/(dashboard)/dashboard/knowledge-center/page.js
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
-import { AcademicCapIcon, BookOpenIcon, CheckCircleIcon, LinkIcon, PlusCircleIcon, XCircleIcon } from "@heroicons/react/24/outline";
+import { AcademicCapIcon, BookOpenIcon, BriefcaseIcon, CheckCircleIcon, LinkIcon, PlusCircleIcon, XCircleIcon } from "@heroicons/react/24/outline";
 import DataTable from "react-data-table-component";
 import SkeletonLoader from "@/app/components/Skeleton/Skeleton";
 import TextField from "@/app/components/Common/Input/TextField";
@@ -18,6 +18,7 @@ import Link from "next/link";
 import TopBar from "@/app/components/Common/Card/TopBar";
 import TextArea from "@/app/components/Common/Input/TextArea";
 import { isMobile } from "react-device-detect";
+import SideModal from "@/app/components/SideModal/SideModal";
 
 const Page = () => {
     const workflowState = useSelector(state => state.workflow);
@@ -35,29 +36,23 @@ const Page = () => {
     const [workflow, setWorkflow] = useState([])
     const [recommendationOrderBy, setRecommendationOrderBy] = useState('');
     const [search, setSearch] = useState('');
+    const [searchKnowledge, setSearchKnowledge] = useState('');
     const [typingTimeout, setTypingTimeout] = useState(null);
-    console.log("state", state);
+
+    const [workflowView, setWorkflowView] = useState(null)
+    const [show, setShow] = useState(null)
+    const [knowledgeId, setKnowledgeId] = useState(null)
+    const [answer, setAnswer] = useState('')
+
     const getData = async () => {
         setTabLoader(true);
         const response = await getKnowledgeData()
         if (response?.data?.results.length > 0) {
             setKnowledge(response?.data?.results)
-
-            const botDataArray = response?.data?.results.map(entry => {
-                if (entry.bots.length === 0) {
-                    return []; // Return an empty array for entries with no bots
-                } else {
-                    return entry.bots.map(bot => ({
-                        value: bot.bot.id,
-                        name: bot.bot.chat_title,
-                    }));
-                }
-            });
-            setBasicFormData(prev => {
+            setBasicFormData((prev) => {
                 return {
                     ...prev,
-                    selectedBot: botDataArray,
-                    knowledgeData: response?.data?.results
+                    knowledge: response?.data?.results
                 }
             })
             setTabLoader(false);
@@ -171,6 +166,10 @@ const Page = () => {
 
 
     }
+
+
+
+
     const divRef = useRef(null);
     useEffect(() => {
         const handleOutsideClick = (event) => {
@@ -213,22 +212,22 @@ const Page = () => {
                 // padding: '10px 0 10px 0'
             },
         },
-        {
-            name: "New Answer",
-            selector: 'question',
-            // sortable: true,
-            reorder: true,
-            cell: (row) => (
-                <div className='w-[100%]'>
-                    <TextArea name="interrogatory_type"
-                        className="py-2"
-                        type={"text"}
-                        id={"interrogatory_type"}
-                        onChange={(e) => handleInput({ ...row, answer: e.target.value })}
-                        value={row.answer} />
-                </div>
-            ),
-        },
+        // {
+        //     name: "New Answer",
+        //     selector: 'question',
+        //     // sortable: true,
+        //     reorder: true,
+        //     cell: (row) => (
+        //         <div className='w-[100%]'>
+        //             <TextArea name="interrogatory_type"
+        //                 className="py-2"
+        //                 type={"text"}
+        //                 id={"interrogatory_type"}
+        //                 onChange={(e) => handleInput({ ...row, answer: e.target.value })}
+        //                 value={row.answer} />
+        //         </div>
+        //     ),
+        // },
         {
             name: "Count",
             selector: 'number_of_messages',
@@ -247,26 +246,17 @@ const Page = () => {
                         row?.accepted === false && (
                             <>
 
-                                {updateLoader === row.id ?
-                                    <ColorRing
-                                        height="30"
-                                        width="30"
-                                        color="#4fa94d"
-                                        ariaLabel="tail-spin-loading"
-                                        radius="1"
-                                        wrapperClass="text-center"
-                                        visible={true}
-                                    /> :
-                                    <div>
-                                        <button type="button" onClick={(e) => {
-                                            if (checkValue(row.answer) === true) {
-                                                updateButtonHandler(row.id)
-                                            }
-                                        }}>
-                                            <CheckCircleIcon className={`${checkValue(row.answer) === false ? "text-border" : "text-success"} h-6 w-6 `} />
-                                        </button>
-                                    </div>
-                                }
+
+                                <div>
+                                    <button type="button" onClick={(e) => {
+                                        setWorkflowView(row)
+                                        console.log("row", row)
+                                        setShow(true)
+                                    }}>
+                                        <PlusCircleIcon className="h-6 w-6 text-success " />
+                                    </button>
+                                </div>
+
                                 {deleteLoader === row.id ?
                                     <ColorRing
                                         height="30"
@@ -281,7 +271,7 @@ const Page = () => {
                                         <button type="button" onClick={(e) => deleteButtonHandler(row.id)}>
                                             <XCircleIcon className="h-6 w-6 text-danger " /></button></div>
                                 }
-                                <ButtonComponent row={row} handleWorkflow={handleWorkflow} workflow={workflow} index={index} total={state?.data?.results?.length} />
+                                {/* <ButtonComponent row={row} handleWorkflow={handleWorkflow} workflow={workflow} index={index} total={state?.data?.results?.length} /> */}
 
                             </>
                         )}
@@ -367,7 +357,21 @@ const Page = () => {
             setLoading(false)
         }
     };
-
+    const searchKnowledgeData = (e) => {
+        const { value, name } = e.target;
+        setSearchKnowledge(value);
+      
+        const filteredKnowledge = basicFormData.knowledge.filter((x) => {
+          if (x.title.includes(value)) {
+            return true; // Include this item in the filtered result
+          } else {
+            return false; // Exclude this item from the filtered result
+          }
+        });
+      
+        setKnowledge(filteredKnowledge);
+    }
+    console.log("knowledge", knowledge)
     return (
         <>
             <div style={{ whiteSpace: "normal" }}>
@@ -429,8 +433,93 @@ const Page = () => {
                         />
                     </div>
                 </>
-                {/* )} */}
-            </div>
+                {workflowView && show && (
+                    <SideModal setShow={setShow} heading={<p className="w-full sm:w-[80%]">{workflowView?.question}</p>}>
+                        <div className="border-b border-border dark:border-gray-700 flex items-center justify-between mt-5">
+                            <ul className="flex flex-nowrap items-center overflow-x-auto sm:flex-wrap -mb-px text-xs font-medium text-center text-gray-500">
+                                <li className="mr-2" onClick={() => { setTab(0) }}>
+                                    <span
+                                        className={`flex justify-start text-xs gap-2 cursor-pointer items-center py-2  ${tab === 0 && ("border-b-2 text-primary border-primary")}  font-bold  rounded-t-lg active  group`}
+                                        aria-current="page"
+                                    >
+                                        <AcademicCapIcon className="h-5 w-5 text-gray-500" /> Add to Knowledge Base
+                                    </span>
+                                </li>
+                                <li className="mr-2" onClick={() => { setTab(1) }}>
+                                    <span
+                                        className={`flex justify-start gap-2 text-xs  cursor-pointer items-center py-2   ${tab === 1 && (" border-b-2  text-primary border-primary")}  font-bold rounded-t-lg active pl-2 group`}
+                                        aria-current="page"
+                                    >
+                                        <BriefcaseIcon className="h-5 w-5 text-gray-500" /> Trigger Workflow
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
+                        {tab === 0 && (
+                            <div>
+                                <div className=' mt-2 '>
+                                    <label htmlFor="search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                                    <div className="relative">
+                                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                            </svg>
+                                        </div>
+                                        <input type="search" id="search" className="border border-input_color w-full block  px-2 py-2 bg-white focus:bg-white  rounded-md shadow-sm placeholder-slate-400  focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50  invalid:border-pink-500  focus:invalid:border-pink-500 focus:invalid:ring-pink-500 pl-10" placeholder="Search" value={searchKnowledge} onChange={(e) => { searchKnowledgeData(e) }} />
+                                    </div>
+                                </div>
+
+                                {!knowledgeId && (
+                                    <div className={` bg-[#F8F8F8] my-4`}>
+                                        <ul className="py-2 text-sm text-gray-700 ">
+                                            {knowledge.map((element, key) =>
+                                                <li className='hover:bg-primary hover:text-white text-heading my-2 cursor-pointer' key={key} onClick={(e) => { setKnowledgeId(element) }}>
+                                                    <button type='button' className="block px-4 py-2 text-xs">{makeCapital(element.title)}</button>
+                                                </li>
+                                            )}
+                                        </ul>
+                                    </div>
+                                )}
+                                {knowledgeId && (
+                                    <div>
+                                        <div className={` bg-[#F8F8F8] my-4 p-4`}>
+                                            <p className="text-xs">{knowledgeId.title}</p>
+                                        </div>
+                                        <div className=''>
+                                            <TextArea name="answer"
+                                                className="py-2"
+                                                type={"text"}
+                                                id={"answer"}
+                                                placeholder={""}
+                                                onChange={(e) => setAnswer(e.target.value)}
+                                                value={answer} />
+                                        </div>
+                                        <button onClick={(e) => console.log("yes")} type="button" className="my-6 flex items-center justify-center text-xs gap-1 focus:ring-4 focus:outline-none font-bold rounded-md py-2.5 px-4 w-auto focus:ring-yellow-300 bg-primary  text-white hover:shadow-[0_8px_9px_-4px_#0000ff8a] disabled:bg-input_color disabled:shadow-none disabled:text-white">
+                                            Submit
+                                        </button>
+                                    </div>
+                                )}
+                            </div>
+                        )}
+                        {tab === 1 && (
+                            <div className={` bg-[#F8F8F8] my-4`}>
+                                {workflow.length > 0 ?
+                                    <ul className="py-2 text-sm text-gray-700 ">
+                                        {workflow.map((ele, key) =>
+                                            <li className='hover:bg-primary hover:text-white text-heading my-2 cursor-pointer' key={key}>
+                                                <button type='button' className="block px-4 py-2 ">{makeCapital(ele.name)}</button>
+                                            </li>
+
+                                        )}
+                                        <li className='hover:bg-primary hover:text-white text-heading my-2' >
+                                            <Link href={'/dashboard/workflow/workflow-builder'}><button type='button' className="block px-4 py-2 ">Add New Workflow </button></Link>
+                                        </li>
+                                    </ul> : <small>No data found!</small>}
+                            </div>
+                        )}
+                    </SideModal>
+                )}
+            </div >
             <ToastContainer />
         </>
     );
@@ -440,51 +529,3 @@ export default Page;
 
 
 
-
-export const ButtonComponent = ({ row, handleWorkflow, workflow, index, total }) => {
-    const divRef = useRef(null);
-    const [openWorkflows, setOpenWorkflow] = useState(null)
-    useEffect(() => {
-        const handleOutsideClick = (event) => {
-            if (divRef.current && !divRef.current.contains(event.target)) {
-                setOpenWorkflow(null);
-            }
-        };
-        document.addEventListener("click", handleOutsideClick);
-
-        return () => {
-            document.removeEventListener("click", handleOutsideClick);
-        };
-    }, []);
-
-    return (
-        <>
-            <div className='cursor-pointer relative' ref={divRef} onClick={(e) => setOpenWorkflow(prev => prev === row.id ? null : row.id)}>
-                <button type="button">
-                    <PlusCircleIcon className="h-6 w-6 text-success " />
-                    {/* {index} */}
-                </button>
-                <div className="relative" >
-
-                    {openWorkflows === row.id && (
-                        <div className={`absolute left-[-315px]  ${(index === total - 1 || index === total - 2 || index === total - 3) ? 'top-[unset] bottom-[50px]' : 'top-[34px]'}   sm:left-[-280px] md:left-[-280px] lg:left-[-280px]  z-10 bg-[#F8F8F8] divide-y divide-gray-100 min-w-[300px] border border-border rounded-lg shadow w-44`}>
-                            {workflow.length > 0 ?
-                                <ul className="py-2 text-sm text-gray-700 ">
-                                    {workflow.map((ele, key) =>
-                                        <li className='hover:bg-primary hover:text-white text-heading my-2' key={key} onClick={() => handleWorkflow(ele, row.id)}>
-                                            <button type='button' className="block px-4 py-2 ">{makeCapital(ele.name)}</button>
-                                        </li>
-
-                                    )}
-                                    <li className='hover:bg-primary hover:text-white text-heading my-2' >
-                                        <Link href={'/dashboard/workflow/workflow-builder'}><button type='button' className="block px-4 py-2 ">Add New Workflow </button></Link>
-                                    </li>
-                                </ul> : <small>No data found!</small>}
-                        </div>
-                    )}
-                </div>
-            </div>
-        </>
-    )
-
-}
