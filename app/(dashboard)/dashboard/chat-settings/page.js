596f2961be18e39d35e9ef98ec2741fe471ac289
Vivek <vivek@usetempo.ai>
2023-10-01 23:54:48 +0530

    Changes

--- app/(dashboard)/dashboard/chat-settings/page.js
+++ app/(dashboard)/dashboard/chat-settings/page.js
@@ -232,8 +232,8 @@ const page = () => {
                         </div>
                         :
                         <>
-                            <Customize form={false} basicFormData={basicFormData} setBasicFormData={setBasicFormData} />
-                            <div className='flex justify-end items-center px-6 py-4'>
+                            <Customize form={false} basicFormData={basicFormData} setBasicFormData={setBasicFormData} buttonLoading={loading} DisablingButton={DisablingButton} SubmitForm={SubmitForm} />
+                            {/* <div className='flex justify-end items-center px-6 py-4'>
                                 {
                                     loading ? (
                                         <LoaderButton />
@@ -249,7 +249,7 @@ const page = () => {
                                             </Button>
                                         </>
                                     )}
-                            </div>
+                            </div> */}
                         </>
 
                     }
