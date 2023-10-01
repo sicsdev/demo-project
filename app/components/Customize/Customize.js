0fa138b0b9b24192af3e29937edf4551c70039a8
Vivek <vivek@usetempo.ai>
2023-10-01 23:55:20 +0530

    Changes

--- app/components/Customize/Customize.js
+++ app/components/Customize/Customize.js
@@ -805,12 +805,12 @@ const Customize = ({ form = false, basicFormData, setBasicFormData, buttonLoadin
                 <div className='flex justify-end items-center px-6 py-4'>
                   {
                     buttonLoading ? (
-                      <LoaderButton className={`mt-2 px-6 pb-2 pt-2 text-xs font-medium w-auto sm:w-[130px]`} />
+                      <LoaderButton className={`mt-2 px-6 pb-2 pt-2 text-xs font-medium`} />
                     ) : (
                       <>
                         <Button
                           type={"button"}
-                          className="inline-block rounded bg-primary mt-2 px-6 pb-2 pt-2 text-xs font-medium  leading-normal text-white disabled:shadow-none  transition duration-150 ease-in-out hover:bg-success-600 hover:shadow-[0_8px_9px_-4px_#0000ff8a] focus:bg-success-600 focus:shadow-[0_8px_9px_-4px_#0000ff8a] focus:outline-none focus:ring-0 active:bg-success-700 active:shadow-[0_8px_9px_-4px_#0000ff8a] w-auto sm:w-[130px]"
+                          className="inline-block rounded bg-primary mt-2 px-6 pb-2 pt-2 text-xs font-medium  leading-normal text-white disabled:shadow-none  transition duration-150 ease-in-out hover:bg-success-600 hover:shadow-[0_8px_9px_-4px_#0000ff8a] focus:bg-success-600 focus:shadow-[0_8px_9px_-4px_#0000ff8a] focus:outline-none focus:ring-0 active:bg-success-700 active:shadow-[0_8px_9px_-4px_#0000ff8a]"
                           disabled={DisablingButton()}
                           onClick={(e) => SubmitForm()}
                         >
