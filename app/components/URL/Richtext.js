66523539b443f00b4e981a25a19d9a895bfcdb38
zkcastTMP <134722001+zkcastTMP@users.noreply.github.com>
2023-10-06 20:02:07 +0200

    test1

--- app/components/URL/Richtext.js
+++ app/components/URL/Richtext.js
@@ -1,23 +1,55 @@
-import React, { useState } from 'react'
-import { EditorState } from 'draft-js'
-import { Editor } from 'react-draft-wysiwyg'
+import React, { useState } from 'react';
+import { Editor } from 'react-draft-wysiwyg';
+import { EditorState, convertToRaw, ContentState, convertFromHTML } from 'draft-js';
+import draftToHtml from 'draftjs-to-html';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
+const TextEditor = ({ oldContent, editing }) => {
 
-function App() {
-  const [editorState, setEditorState] = useState(() =>
-    EditorState.createEmpty(),
-  )
-  const toolbarOptions = {
-    options: ["link"],  
+  const [editorState, setEditorState] = useState(() => {
+    // if (oldContent) {
+    //     const blocksFromHtml = convertFromHTML(oldContent);
+    //     const state = ContentState.createFromBlockArray(blocksFromHtml.contentBlocks);
+    //     return EditorState.createWithContent(state);
+    // }
+    return EditorState.createEmpty();
+  });
+
+  const [postContent, setPostContent] = useState('')
+
+  const onEditorStateChange = (editorState) => {
+    setEditorState(editorState);
+    setPostContent(draftToHtml(convertToRaw(editorState.getCurrentContent())));
   };
+
+
   return (
-    <div className="App">
-      <header className="App-header">Rich Text Editor Example</header>
-
-      <Editor editorState={editorState} toolbar={toolbarOptions} onEditorStateChange={setEditorState} />
+    <div>
+      <Editor
+        editorState={editorState}
+        toolbarClassName="toolbarClassName"
+        wrapperClassName="wrapperClassName"
+        editorClassName="editorClassName"
+        onEditorStateChange={onEditorStateChange}
+        toolbar={{
+          options: ['inline', 'fontSize', 'fontFamily', 'colorPicker', 'emoji', 'link'],
+          inline: {
+            options: ['bold', 'italic', 'underline'],
+          },
+          fontSize: {
+            options: [8, 9, 10, 11, 12, 14, 16, 18, 24],
+          },
+          fontFamily: {
+            options: ['Arial', 'Georgia', 'Impact', 'Tahoma', 'Times New Roman', 'Verdana'],
+          },
+          link: {
+            options: ['link', 'unlink'],
+          },
+        }}
+        placeholder="Que deseas publicar?"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default TextEditor;
