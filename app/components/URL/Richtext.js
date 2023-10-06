f62100bc7a1ebb4b2d44a5bfe784882b043ce1c2
zkcastTMP <134722001+zkcastTMP@users.noreply.github.com>
2023-10-06 21:33:02 +0200

    Update Richtext.js

--- app/components/URL/Richtext.js
+++ app/components/URL/Richtext.js
@@ -8,18 +8,23 @@ import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 const TextEditor = ({ oldContent, editing, handleTextEditorChange, debugMode }) => {
 
   const [editorState, setEditorState] = useState(() => {
-    // if (oldContent) {
-    //     const blocksFromHtml = convertFromHTML(oldContent);
-    //     const state = ContentState.createFromBlockArray(blocksFromHtml.contentBlocks);
-    //     return EditorState.createWithContent(state);
-    // }
+    if (oldContent) {
+      const blocksFromHtml = convertFromHTML(oldContent);
+      const state = ContentState.createFromBlockArray(blocksFromHtml.contentBlocks);
+      console.log(oldContent, 'oldcontent')
+      console.log(blocksFromHtml, 'blocks')
+      console.log('state', state)
+      return EditorState.createWithContent(restoreLinks(state));
+    }
     return EditorState.createEmpty();
   });
 
+  const [postContentWithOutReplace, setPostContentWithOutReplace] = useState('')
   const [postContent, setPostContent] = useState('')
 
   const onEditorStateChange = (editorState) => {
     setEditorState(editorState);
+    setPostContentWithOutReplace(draftToHtml(convertToRaw(editorState.getCurrentContent())));
     setPostContent(replaceLink(draftToHtml(convertToRaw(editorState.getCurrentContent()))));
     handleTextEditorChange(replaceLink(draftToHtml(convertToRaw(editorState.getCurrentContent()))));
   };
@@ -27,11 +32,11 @@ const TextEditor = ({ oldContent, editing, handleTextEditorChange, debugMode }) 
   const showOutput = () => {
     console.log(postContent, 'postcontent')
     console.log(editorState, 'editor state')
+    console.log(postContentWithOutReplace, 'without format')
   }
 
 
   const customStyleMap = {
-    // Estilo para los enlaces
     LINK: {
       color: 'blue',
       fontWeight: 'bold',
@@ -44,7 +49,7 @@ const TextEditor = ({ oldContent, editing, handleTextEditorChange, debugMode }) 
   function replaceLink(html) {
     let pattern = /<a\s+[^>]*href="([^"]+)"[^>]*>([^<]+)<\/a>/g;
     let matches = html.match(pattern);
-  
+
     if (matches) {
       matches.forEach(function (match) {
         let parts = pattern.exec(match);
@@ -54,10 +59,30 @@ const TextEditor = ({ oldContent, editing, handleTextEditorChange, debugMode }) 
         html = html.replace(match, newFormat);
       });
     }
-  
+
+    html = html.replace(/^<p>/, '').replace(/<\/p>$/, '');
     return html;
   }
-  
+
+
+  function restoreLinks(html) {
+    let pattern = /\[([^:]+):([^]+)\]/g;
+    let matches = html.match(pattern);
+
+    if (matches) {
+      matches.forEach(function (match) {
+        let parts = pattern.exec(match);
+        let name = parts[1];
+        let url = parts[2];
+        let newFormat = '<a href="' + url + '" target="_self">' + name + '</a>';
+        html = html.replace(match, newFormat);
+      });
+    }
+
+    return html;
+  }
+
+
 
   // uses example
   // let originalHtml = '<p><a href="www.usetempo.ai" target="_self">Use Tempo</a>&nbsp;</p>';
