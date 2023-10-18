92cb8d2da3ec67b211b412a7ee61f97fb7b1a94f
joywithyash <yash@usetempo.ai>
2023-10-19 01:16:59 +0530

    bot fixes , cookies issue , ui changes

--- app/components/Dashboard/ChatBots.js
+++ app/components/Dashboard/ChatBots.js
@@ -13,9 +13,15 @@ import { createEnterpriseAccount } from '@/app/API/pages/EnterpriseService';
 import { useRouter } from 'next/navigation';
 import SideModal from '@/app/components/SideModal/SideModal';
 import { useEffect } from 'react';
+import TextField from '../Common/Input/TextField';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { fetchBot } from '../store/slices/botIdSlice';
 
 
 const ChatBots = ({ setSkeleton, skeleton }) => {
+    const state = useSelector(state => state.user)
+    const dispatch = useDispatch()
     const router = useRouter()
     const [showModal, setShowModal] = useState(false)
     const [errors, setErrors] = useState([])
@@ -30,7 +36,7 @@ const ChatBots = ({ setSkeleton, skeleton }) => {
             "description": "",
             "automation_tolerance": 0,
             "logo": "",
-            "chat_title": 'Tempo Agent',
+            "chat_title": basicFormData?.bot_name,
             "payment_platform": "Order",
             "ticketing_platform": "Other",
             "cancellation_tolerance": 0,
@@ -39,47 +45,31 @@ const ChatBots = ({ setSkeleton, skeleton }) => {
         }
         const bot = await createBot(payload);
         if (bot?.status === 201) {
-            const bot_faq = await createBotKnowledge(bot.data.id, { urls: basicFormData.urls });
-            const enterprise = await createEnterpriseAccount({ slug_domain: basicFormData.company_name })
-            if (bot_faq?.status === 201 && enterprise?.status === 201) {
-                let email_payload = {
-                    email: basicFormData.email_prefix + "@" + basicFormData.company_name + '.gettempo.ai',
-                    agent_name: basicFormData.agent_name,
-                    email_agent_title: basicFormData.agent_title,
-                    email_greeting: basicFormData.email_introduction,
-                    email_farewell: basicFormData.email_signOff,
-                }
-                const verify = await modifyBot(bot.data.id, email_payload)
-                if (verify.status === 200) {
-                    setBasicFormData((prev) => {
-                        return {
-                            ...prev,
-                            configure: "success"
-                        }
-                    })
-                    router.push(`/customize?id=${bot.data.id}&name=`)
-                    setShowModal(false)
-                    setLoading(false)
-                } else {
-                    setErrors(domains.response.data.slug_domain)
-                    setLoading(false)
-                }
+            const enterprise = await createEnterpriseAccount({ slug_domain: state?.enterprise?.domain })
+            if (enterprise?.status === 201 || enterprise?.status === 200) {
+                debugger
+                setBasicFormData((prev) => {
+                    return {
+                        ...prev,
+                        configure: "success"
+                    }
+                })
+                dispatch(fetchBot())
+                router.push(`/dashboard/chat-settings?id=${bot.data.id}`)
+                setShowModal(false)
+                setLoading(false)
             } else {
-
                 setLoading(false)
             }
+        } else {
+            setLoading(false)
+
         }
     }
 
     const DisablingButton = () => {
-        const bot_data = ['urls'].every(key => !basicFormData[key] || basicFormData[key].length === 0);
-        const email_config = ["email_prefix", "custom_email", "company_name"].some(key => !basicFormData[key] || basicFormData[key].trim() === '');
-        const email_agent = [
-            'agent_title',
-            'email_introduction',
-            'email_signOff'].some(key => !basicFormData[key] || basicFormData[key].trim() === '');
-        const arr_values = ['agent_name'].every(key => !basicFormData[key] || basicFormData[key].length === 0);
-        if (email_agent || arr_values || email_config || bot_data) {
+        const bot_name = ["bot_name"].some(key => !basicFormData[key] || basicFormData[key].trim() === '');
+        if (bot_name) {
             return true
         }
         return false
@@ -91,6 +81,15 @@ const ChatBots = ({ setSkeleton, skeleton }) => {
             setSkeltonLoading(false);
         }, 2500);
     }, []);
+    const handleInputValues = (event) => {
+        const { value, name } = event.target
+        setBasicFormData((prev) => {
+            return {
+                ...prev,
+                [name]: value
+            }
+        })
+    }
 
     return (
         <>
@@ -124,17 +123,17 @@ const ChatBots = ({ setSkeleton, skeleton }) => {
 
                             </div>
                             <div className="flex items-center gap-4 ">
-                            {skeltonLoading ?
-                                <SkeletonLoader count={1} height={30} width={100} />
-                                :
-                                <button
-                                    onClick={(e) => { setShowModal(true) }}
-                                    className="flex items-center gap-2 justify-center font-semibold bg-white text-xs px-5 pb-2 pt-2 border-[#F0F0F1] leading-normal text-[#151D23] disabled:shadow-none hover:bg-primary hover:text-[#ffffff] transition duration-150 ease-in-out focus:outline-none focus:ring-0 active:bg-success-700 border-[1px] rounded-lg  "
+                                {skeltonLoading ?
+                                    <SkeletonLoader count={1} height={30} width={100} />
+                                    :
+                                    <button
+                                        onClick={(e) => { setShowModal(true) }}
+                                        className="flex items-center gap-2 justify-center font-semibold bg-white text-xs px-5 pb-2 pt-2 border-[#F0F0F1] leading-normal text-[#151D23] disabled:shadow-none hover:bg-primary hover:text-[#ffffff] transition duration-150 ease-in-out focus:outline-none focus:ring-0 active:bg-success-700 border-[1px] rounded-lg  "
 
-                                >
-                                    Create New Agent
-                                </button>
-                            }
+                                    >
+                                        Create New Agent
+                                    </button>
+                                }
                             </div>
                         </div>
                     )
@@ -144,9 +143,23 @@ const ChatBots = ({ setSkeleton, skeleton }) => {
 
                 {showModal === true ?
                     <SideModal setShow={setShowModal} heading={'Create New Agent'} >
-                        <CustomerServiceSetupForm form={false} setBasicFormData={setBasicFormData} basicFormData={basicFormData} />
-                        <EmailConfig form={false} setBasicFormData={setBasicFormData} basicFormData={basicFormData} />
-                        <EmailAgentSetting form={false} setBasicFormData={setBasicFormData} basicFormData={basicFormData} />
+                        <div className="my-2">
+                            <TextField
+                                onChange={handleInputValues}
+                                value={basicFormData?.bot_name}
+                                name="bot_name"
+                                className="py-3 mt-1"
+                                title={
+                                    <div className="flex items-center gap-2 w-[150px]">
+                                        <span>Bot Name</span>{" "}
+                                    </div>
+                                }
+                                placeholder={"Bot Name"}
+                                type={"text"}
+                                id={"bot_name"}
+                                error={''}
+                            />
+                        </div>
 
                         {errors.length > 0 && errors.map((ele, key) => <p className='text-danger text-xs' key={key}>{ele}</p>)}
                         <div className={`flex  p-2 rounded-b mt-5  justify-end`}>
