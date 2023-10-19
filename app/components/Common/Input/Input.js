d395156aefcd95dd1326eb001e9568ddc1e39832
joywithyash <yash@usetempo.ai>
2023-10-20 00:33:21 +0530

    changes

--- app/components/Common/Input/Input.js
+++ app/components/Common/Input/Input.js
@@ -12,7 +12,7 @@ export const Input = ({ id, placeholder, onkeydown, className, onChange, value, 
                 onKeyDown={onkeydown}
                 placeholder={placeholder || ""}
                 value={value}
-                className={`block !text-[16px] sm:text-[12px]   px-3 py-3 bg-white focus:bg-white focus:text-sm rounded-md text-sm shadow-sm placeholder-slate-400  focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 ${className || ''}`}
+                className={`block  sm:text-[12px]   px-3 py-3 bg-white focus:bg-white focus:text-sm rounded-md text-sm shadow-sm placeholder-slate-400  focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 ${className || ''}`}
                 onChange={(e) => handleChange(e)}
                 {...rest}
                 disabled={disabled || false} />
