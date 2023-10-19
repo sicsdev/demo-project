178f6dc7f17c88d427e4cf34a398477b947a7aef
viveks5898 <singh.vivek5898@gmail.com>
2023-10-19 08:54:11 -0700

    skelton

--- app/components/LayoutNew/Homeintegration.js
+++ app/components/LayoutNew/Homeintegration.js
@@ -1,44 +1,56 @@
 "use client";
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import integration from "../../data/newInt.json";
 import { useRouter, useSearchParams } from "next/navigation";
 import Image from "next/image";
-import Link from "next/link";
+import SkeletonLoader from "../Skeleton/Skeleton";
 const Homeintegration = (props) => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const [data, setData] = useState(integration);
+  const [loading, setLoading] = useState(true);
 
+  const [data, setData] = useState(integration);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div className="bg-white  sm:p-[0px] ">
-
-
-      <div className="w-full m-auto flex justify-center sm:my-[50px]">
-
-      </div>
-      <h1 className="mb-5  sm:p-0 sm:mt-0 mt-5 text-black text-center text-2xl sm:text-[38px] font-bold sm:mb-7">
-      Tempo Integration Ecosystem      </h1>
+      <div className="w-full m-auto flex justify-center sm:my-[50px]"></div>
+      {loading ? (
+        <div className="mb-5  sm:p-0 sm:mt-0 mt-5 text-black text-center text-2xl sm:text-[38px] font-bold sm:mb-7">
+          <SkeletonLoader height={60} width={"50%"} />
+        </div>
+      ) : (
+        <h1 className="mb-5  sm:p-0 sm:mt-0 mt-5 text-black text-center text-2xl sm:text-[38px] font-bold sm:mb-7">
+          Tempo Integration Ecosystem{" "}
+        </h1>
+      )}
+          {loading ? (
+        <div className="text-blue-400 w-full md-w-[339px] text-center font-[400] text-heading  justify-center text-[16px] leading-[22px] sm:text-[24px] sm:leading-8 gap-2">
+          <SkeletonLoader height={60} width={"100%"} />
+        </div>
+      ) : (
       <p className="text-blue-400 w-full md-w-[339px] text-center font-[400] text-heading xs:flex-row xs:flex-col sm:flex justify-center text-[16px] leading-[22px] sm:text-[24px] sm:leading-8 gap-2">
-      Enhance your setup with APIs that integrate effortlessly into the Tempo platform, meeting your unique business needs.
+        Enhance your setup with APIs that integrate effortlessly into the Tempo
+        platform, meeting your unique business needs.
       </p>
+      )}
 
       <div className="sm:pt-[10px] bg-white">
-      <div className="pt-8 pb-8 sm:pb-4">
-        <div className="mx-auto max-w-[90%] sm:max-w-[90%] md:max-w-[90%] lg:max-w-[90%]">
-          {data.length === 0 ? (
-            <p className=" text-xl align-bottom font-semibold italic text-center">
-              No data found!
-            </p>
-          ) : (
-            ""
-          )}
-            <div
-              className="max-w-[1400px] w-full m-auto sm:py-4 sm:px-4 px-0 lg:px-4 relative group"
-            >
-          
-              <div className="grid grid-cols-4 lg:grid-cols-8 gap-6 w-full m-auto sm:py-8 md:py-8 lg:py-8 sm:px-4 lg:px-4">
+        <div className="pt-8 pb-8 sm:pb-4">
+          <div className="mx-auto max-w-[90%] sm:max-w-[90%] md:max-w-[90%] lg:max-w-[90%]">
+            {data.length === 0 ? (
+              <p className=" text-xl align-bottom font-semibold italic text-center">
+                No data found!
+              </p>
+            ) : (
+              ""
+            )}
+            <div className="max-w-[1400px] w-full m-auto sm:py-4 sm:px-4 px-0 lg:px-4 relative group">
+              <div className="grid grid-cols-4 lg:grid-cols-7 gap-6 w-full m-auto sm:py-8 md:py-8 lg:py-8 sm:px-4 lg:px-4">
                 {data?.map((item, key) => (
                   <div
                     className="bg-white flex flex-col items-center  justify-between cursor-pointer shadow-lg hover:translate-y-[-4px] transition-transform duration-300 "
@@ -49,8 +61,12 @@ const Homeintegration = (props) => {
                     key={key}
                   >
                     <div className="p-2 sm:p-8 md:py-7 md:px-4 ">
-                    {/* <Link href={`${item.link}`} > */}
-
+                      {/* <Link href={`${item.link}`} > */}
+                      {loading ? (
+        <div className="relative mx-auto w-[30px] h-[30px] sm:w-[60px] sm:h-[60px] gap-2 rounded-lg sm:m-auto mb-5">
+          <SkeletonLoader className=" w-[30px] h-[30px] sm:w-[60px] sm:h-[60px] " />
+        </div>
+      ) : (
                       <div className="relative mx-auto w-[30px] h-[30px] sm:w-[60px] sm:h-[60px] gap-2 rounded-lg sm:m-auto mb-5">
                         <Image
                           fill={"true"}
@@ -59,16 +75,15 @@ const Homeintegration = (props) => {
                           src={item.logo}
                         />
                       </div>
+      )}
                       {/* </Link> */}
-            
                     </div>
-                
                   </div>
                 ))}
               </div>
             </div>
+          </div>
         </div>
-      </div>
       </div>
     </div>
   );
--- app/components/LayoutNew/Reach.js
+++ app/components/LayoutNew/Reach.js
@@ -1,5 +1,5 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import SkeletonLoader from "../Skeleton/Skeleton";
 const Reach = () => {
   const data = [
     {
@@ -20,49 +20,79 @@ const Reach = () => {
       name: "24/7/365 Uptime ",
       para: "Benefit from round-the-clock system availability, ensuring uninterrupted service for your customers.  ",
       link_title: "Leverage your data",
-    }
-
+    },
   ];
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <div className="bg-white p-0 sm:p-[0px] sm:mb-12  shadow-box">
-      <h1 className="mb-5 sm:mt-0 mt-5 text-black px-3  sm:text-center text-2xl sm:text-[38px] font-bold sm:mb-0">
-      Exceptional Performance      </h1>
-
+        {loading ? (
+        <div className="mb-5  sm:p-0 sm:mt-0 mt-5 text-black text-center text-2xl sm:text-[38px] font-bold sm:mb-7">
+          <SkeletonLoader height={60} width={"50%"} />
+        </div>
+      ) : (
+        <h1 className="mb-5 sm:mt-0 mt-5 text-black px-3  sm:text-center text-2xl sm:text-[38px] font-bold sm:mb-0">
+          Exceptional Performance{" "}
+        </h1>
+      )}
 
       <div className="sm:pt-[0px] bg-white">
-          <div className="max-w-[90%] sm:max-w-[100%]">
-            {data.length === 0 ? (
-              <p className=" text-xl align-bottom font-semibold italic text-center">
-                No data found!
-              </p>
-            ) : (
-              ""
-            )}
-            <div className="max-w-[1400px] w-full m-auto sm:py-4 sm:px-4 px-3 lg:px-4 relative group">
-              <div className="grid grid-cols-1 md:grid-cols-2  sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3      gap-5 sm:gap-12  w-full m-auto sm:py-8 md:py-8 lg:py-8 sm:px-4 lg:px-4">
-                {data?.map((ele, key) => (
-                  <div
-                    className="bg-white flex flex-col justify-between cursor-pointer sm:p-[0px] hover:outline-[1px] hover:outline-[black]  transition-transform duration-300 "
-                    key={key}
-                  >
-                    {/* <div className="p-0 text-[50px] font-bold leading-[60px] py-2 sm:p-8 md:py-7 md:px-0 text-[#fe9327] ">
+        <div className="max-w-[90%] sm:max-w-[100%]">
+          {data.length === 0 ? (
+            <p className=" text-xl align-bottom font-semibold italic text-center">
+              No data found!
+            </p>
+          ) : (
+            ""
+          )}
+          <div className="max-w-[1400px] w-full m-auto sm:py-4 sm:px-4 px-3 lg:px-4 relative group">
+            <div className="grid grid-cols-1 md:grid-cols-2  sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3      gap-5 sm:gap-12  w-full m-auto sm:py-8 md:py-8 lg:py-8 sm:px-4 lg:px-4">
+              {data?.map((ele, key) => (
+                <div
+                  className="bg-white flex flex-col justify-between cursor-pointer sm:p-[0px] hover:outline-[1px] hover:outline-[black]  transition-transform duration-300 "
+                  key={key}
+                >
+                  {/* <div className="p-0 text-[50px] font-bold leading-[60px] py-2 sm:p-8 md:py-7 md:px-0 text-[#fe9327] ">
                       {ele.reach}
                     </div> */}
+                  {loading ? (
+                    <div className="p-0 py-2 sm:p-3 md:py-2 md:px-0 font-semibold">
+                      <SkeletonLoader height={60} className="w-[100%] sm:w-[50%]" />
+                    </div>
+                  ) : (
                     <div className="p-0 py-2 sm:p-3 md:py-2 md:px-0 font-semibold ">
                       {ele.name}
                     </div>
+                  )}
+                  {loading ? (
+                    <div className="p-0 py-2 sm:p-3 md:py-2 md:px-0 font-semibold">
+                      <SkeletonLoader height={60}className="w-[100%] sm:w-[50%]"/>
+                    </div>
+                  ) : (
                     <div className="p-0 py-2 sm:p-3 md:py-2 md:px-0 ">
                       {ele.para}
                     </div>
-                    <div className="p-0 py-2 sm:p-8 md:py-7 md:px-0 font-semibold cursor-pointer">
-                    Schedule Demo →
-
+                  )}
+                  {loading ? (
+                    <div className="p-0 py-2 sm:p-3 md:py-2 md:px-0 font-semibold">
+                      <SkeletonLoader height={60} className="w-[100%] sm:w-[50%]" />
                     </div>
-                  </div>
-                ))}
-              </div>
+                  ) : (
+                    <div className="p-0 py-2 sm:p-8 md:py-7 md:px-0 font-semibold cursor-pointer">
+                      Schedule Demo →
+                    </div>
+                  )}
+                </div>
+              ))}
             </div>
           </div>
+        </div>
       </div>
     </div>
   );
