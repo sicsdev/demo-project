7f7f3e379794b2384a765e157f1133faca4fb0c8
joywithyashh <yash@usetempo.ai>
2023-11-04 04:23:16 +0530

    design

--- app/components/Layout/data/navData.js
+++ app/components/Layout/data/navData.js
@@ -1,3 +1,6 @@
+import { ComputerDesktopIcon } from "@heroicons/react/24/outline";
+import { Cog8ToothIcon } from "@heroicons/react/24/outline";
+
 export const nav_links = [
     {
         name: "Solutions",
@@ -5,54 +8,55 @@ export const nav_links = [
         card: {
             links: [
                 {
-                    list_heading: "Chat Support",
-                    data: [
+                    list_heading: "Platform",
+                    icon: <ComputerDesktopIcon class="h-6 w-6 text-gray-500" />,      
+                                  data: [
                         {
-                            icon: "/menu/7.png",
-                            "heading": "Tempo Chat",
+                
+                            "heading": "Chat Bot",
                             label: "",
                             link: "solutions/tempo-chat",
                             "para": "Spin up a GPT4 powered bot in minutes to answer common CS needs."
                         },
                         {
                             icon: "/menu/6.png",
-                            "heading": "Smart Inbox",
+                            "heading": "Email",
+                            label: "",
+                            link: "/solutions/smart-inbox",
+                            "para": "Link Tempo into your email support to automatically handle more complex email tickets."
+                        },
+                        {
+                            icon: "/menu/6.png",
+                            "heading": "Phone",
                             label: "",
                             link: "/solutions/smart-inbox",
                             "para": "Link Tempo into your email support to automatically handle more complex email tickets."
                         }
-                        // {
-                        //     icon: "/menu/8.png",
-                        //     "heading": "Smart Social",
-                        //     label: "",
-                        //     link: "/solutions/smart-social",
-                        //     "para":  "Connect Tempo to your social media for swift, AI-powered responses to comments and DMs."
-                        // }
+                  
+                    ]
+                },
+                {
+                    list_heading: "Features",
+                    icon: <Cog8ToothIcon class="h-6 w-6 text-gray-500" />,      
+
+                    data: [
+                        {
+                            icon: "/menu/7.png",
+                            "heading": "Learning Center",
+                            label: "",
+                            link: "solutions/tempo-chat",
+                            "para": "Spin up a GPT4 powered bot in minutes to answer common CS needs."
+                        },
+                        {
+                            icon: "/menu/6.png",
+                            "heading": "Workflow Builder",
+                            label: "",
+                            link: "/solutions/smart-inbox",
+                            "para": "Link Tempo into your email support to automatically handle more complex email tickets."
+                        },
+                      
                     ]
                 }
-                // {
-                //     list_heading: "Voice Support",
-                //     data: [
-                //         {
-                //             icon: "/menu/5.png",
-                //             "heading": "Inbound Smart IVR",
-                //             label: <span className="text-[10px] font-semibold inline-block py-1 px-2  rounded-full text-heading bg-label last:mr-0 mr-1">
-                //                 Coming soon
-                //             </span>,
-                //             link: "/",
-                //             "para": "GPT4-powered phone support for CS needs. "
-                //         },
-                //         {
-                //             icon: "/menu/10.png",
-                //             "heading": "Outbound Agent",
-                //             label: <span className="text-[10px] font-semibold inline-block py-1 px-2  rounded-full text-heading bg-label last:mr-0 mr-1">
-                //                 Coming soon
-                //             </span>,
-                //             link: "/",
-                //             "para": "Make phone calls to customers, leads, or clients automatically powered by GPT4."
-                //         }
-                //     ]
-                // }
             ]
         }
     },
