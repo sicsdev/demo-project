f375867fc40865a29d945e8f26a619cba35ebb7c
zkcastTMP <134722001+zkcastTMP@users.noreply.github.com>
2023-11-08 16:51:15 +0100

    Change: Creation/Edition of products UI

--- app/components/Common/Input/TextField.js
+++ app/components/Common/Input/TextField.js
@@ -1,4 +1,6 @@
+import { InformationCircleIcon } from "@heroicons/react/24/outline";
 import React from "react";
+import { Tooltip } from 'react-tooltip'
 
 const TextField = ({
   labelClassName,
@@ -8,7 +10,7 @@ const TextField = ({
   type,
   placeholder,
   handleInputFocus,
-    id,
+  id,
   register,
   subtitle = null,
   className = "py-3",
@@ -16,13 +18,24 @@ const TextField = ({
   value,
   onBlur,
   paddingleft,
+  tooltipInfo,
   ...rest
 }) => {
   return (
     <div className={`inline  ${labelClassName || ""}`}>
-      <label className={`new_input_label block text-sm text-heading ${labelClass || "font-medium"}`}>
-        {title}
-      </label>
+      <div className='flex items-center mb-1'>
+        <label className={`new_input_label block text-sm text-heading ${labelClass || "font-medium"}`}>
+          {title}
+        </label>
+
+        {tooltipInfo && <InformationCircleIcon
+          data-tooltip-id={title}
+          data-tooltip-content={tooltipInfo}
+          className='w-4 h-4 mx-2'>
+        </InformationCircleIcon>}
+      </div>
+      <Tooltip id={title} place="top" type="dark" effect="solid" />
+
       {subtitle && <small className="text-border text-xs">{subtitle}</small>}
       <input
         value={value}
@@ -32,10 +45,9 @@ const TextField = ({
         type={type || "text"}
         {...register}
         placeholder={placeholder || ""}
-        className={`${className} ${paddingleft} new_input block border-[0.2px]  px-3 bg-white  rounded-md shadow-sm placeholder-slate-400  focus:outline-none focus:border-sky focus:ring-2  disabled:bg-slate-50 disabled:text-slate-500 border-input_color w-full focus:bg-white  ${
-          error &&
+        className={`${className} ${paddingleft} new_input block border-[0.2px]  px-3 bg-white  rounded-md shadow-sm placeholder-slate-400  focus:outline-none focus:border-sky focus:ring-2  disabled:bg-slate-50 disabled:text-slate-500 border-input_color w-full focus:bg-white  ${error &&
           "border-danger invalid:border-danger invalid:text-danger focus:invalid:border-danger focus:invalid:ring-danger focus:ring-danger"
-        }`}
+          }`}
         id={id}
         name={id}
         {...rest}
