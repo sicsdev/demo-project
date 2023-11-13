c2c03c0bdc2885d931b9f0f78d26f6514384e956
zkcastTMP <134722001+zkcastTMP@users.noreply.github.com>
2023-11-13 18:53:10 +0100

    Add: product item in logs

--- app/components/Chats/ProductComponent/ProductComponent.js
+++ app/components/Chats/ProductComponent/ProductComponent.js
@@ -1,18 +1,18 @@
 import React from 'react'
 
-const ProductComponent = () => {
-    
+const ProductComponent = ({ product }) => {
+
     return (
         <div class="">
             <div id='12390123' className="tempo-widget-cart-item-comp">
-                <img src="" alt="Product Image" />
+                <img src={product.image} alt="Product Image" />
                 <div className="tempo-widget-cart-item-comp-content">
                     <h2 className="tempo-widget-cart-item-comp-title">
-                        $title
+                        {product.title}
                     </h2>
-                    <p className="tempo-widget-cart-item-comp-price">$123</p>
-                    <p className="tempo-widget-cart-item-comp-description">description</p>
-                    <a className="tempo-widget-cart-item-comp-link" href='${productUrl}' target='_blank'> <button>Add to Cart</button></a>
+                    <p className="tempo-widget-cart-item-comp-price">{product.price}</p>
+                    <p className="tempo-widget-cart-item-comp-description">{product.description}</p>
+                    <a className="tempo-widget-cart-item-comp-link" href={product.url} target='_blank'> <button>Add to Cart</button></a>
                 </div>
             </div>
         </div>
