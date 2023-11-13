db60857ff92faf07bfb35f3f49fba08ed40f0aaf
AjeetUseTempo <ajeet@mitochondria.co>
2023-11-13 16:12:16 +0530

    add partners page

---
+++ app/(pages)/partners/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Partners from '@/app/components/Partners/Partners'
+const page = () => {
+    return (
+        <>
+            <Partners />
+        </>
+    )
+}
+
+export default page
