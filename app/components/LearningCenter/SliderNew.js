d5aa40e3761dccad0a413ca300b8a17b50fba53c
AjeetUseTempo <ajeet@mitochondria.co>
2023-11-17 09:53:54 +0530

    add new component

--- app/components/LearningCenter/SliderNew.js
+++ app/components/LearningCenter/SliderNew.js
@@ -4,7 +4,7 @@ import { PlusIcon } from "@heroicons/react/24/outline";
 
 
 const SliderNew = () => {
-    const [selectedItemIndex, setSelectedItemIndex] = useState(null);
+    const [selectedItemIndex, setSelectedItemIndex] = useState(0);
 
     const handleClick = (index) => {
         setSelectedItemIndex(selectedItemIndex === index ? null : index);
@@ -50,46 +50,49 @@ const SliderNew = () => {
         return () => {
             clearInterval(intervalId);
         };
-    }, [data.length]);
+    }, [data.length, data]);
 
     return (
         <>
-            <div className='bg-[#f7f8fa] py-10'>
+            <div className='bg-[#f7f8fa] py-10 sm:px-1 px-4'>
                 <div className='text-center mb-20'>
-                    <p className='text-[42px] font-[600]'>Built for security-centered organizations</p>
-                    <p className='text-[16px]'>Adaptable to a wide range of use cases and business needs</p>
+                    <p className='sm:text-[42px] text-3xl sm:font-[600] font-medium'>Built for security-centered organizations</p>
+                    <p className='text-[16px] mt-5'>Adaptable to a wide range of use cases and business needs</p>
                 </div>
-                <div>
-                    {data.map((ele, key) => (
-                        <div className='grid grid-cols-2' key={key}>
-                            <div className='flex justify-end mr-5'>
-                                {selectedItemIndex === key && (
-                                    <div>
-                                        <img src={ele.image} className='h-[400px] w-[400px]' alt={`Image ${key + 1}`} />
-                                    </div>
-                                )}
-                            </div>
+                <div className='grid'>
+                    <div className='sm:grid sm:grid-cols-2 block transition-all duration-500'>
+                        <div className='sm:flex justify-end mr-20 h-auto hidden'>
+
                             <div>
-                                <div className='w-[70%] h-[50px'>
-                                    <div className='flex cursor-pointer text-2xl font-semibold' onClick={() => handleClick(key)}>
-                                        <div>
+                                <img src={data[selectedItemIndex].image} className='h-[700px] w-[500px]' alt={`Image ${selectedItemIndex + 1}`} />
+                            </div>
+
+                        </div>
+                        <div>
+                            {data.map((ele, key) => (
+                                <div className='sm:w-[70%] w-[95%]' key={key}>
+                                    <div className='flex cursor-pointer text-2xl font-normal my-5' onClick={() => handleClick(key)}>
+                                        <div className='text-base mt-1'>
                                             <p>{ele.number}</p>
                                         </div>
                                         <div className='flex justify-between w-[100%]'>
-                                            <p className='ml-3 text-2xl font-semibold'>{ele.title}</p>
+                                            <p className='ml-3 text-2xl font-medium'>{ele.title}</p>
                                             <PlusIcon className={`h-6 w-6 text-gray-500 transform `} />
                                         </div>
                                     </div>
-                                    {selectedItemIndex === key && (
-                                        <div>
-                                            <p>{ele.text}</p>
-                                        </div>
-                                    )}
+                                    <div className={selectedItemIndex === key ? 'h-auto my-3 ml-8 text-text-dark-color' : 'h-[50px] overflow-hidden'}>
+                                        {selectedItemIndex === key ? (
+                                            <div>
+                                                <p>{ele.text}</p>
+                                            </div>
+                                        ) : ""}
+                                    </div>
+                                    <hr className='ml-8' />
                                 </div>
-                            </div>
+                            ))}
+
                         </div>
-                    ))}
-                    <hr />
+                    </div>
                 </div>
             </div>
         </>
