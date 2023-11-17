42414ba5978508036c690e5ba39493d20c882b58
vivekTempo <vivek@usetempo.ai>
2023-11-18 00:08:07 +0530

    fixed home responsiveness

--- app/components/Common/Input/TextField.js
+++ app/components/Common/Input/TextField.js
@@ -48,7 +48,7 @@ const TextField = ({
         type={type || "text"}
         {...register}
         placeholder={placeholder || ""}
-        className={`${className} ${paddingleft} new_input block border-[0.2px]  px-3 bg-white  rounded-md shadow-sm placeholder-slate-400  focus:outline-[0px] focus:border-sky focus:ring-1  disabled:bg-slate-50 disabled:text-slate-500 border-input_color w-full focus:bg-white  ${
+        className={`${className} ${paddingleft} new_input block border-[0.2px]  px-3 bg-white  rounded-md shadow-sm placeholder-slate-400  focus:outline-[0px] focus:border-sky focus:ring-2  disabled:bg-slate-50 disabled:text-slate-500 border-input_color w-full focus:bg-white  ${
           error &&
           "border-danger invalid:border-danger invalid:text-danger focus:invalid:border-danger focus:invalid:ring-danger focus:ring-danger"
         }`}
