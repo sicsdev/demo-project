b95a2cdedb27e044fe637bcd2345bcbfd2f479cd
zkcastTMP <134722001+zkcastTMP@users.noreply.github.com>
2023-11-18 01:14:31 +0100

    Change: array of faq urls to string in /get-trial

--- app/(pages)/get-trial/page.js
+++ app/(pages)/get-trial/page.js
@@ -10,12 +10,12 @@ import { editBillingType } from "@/app/components/store/slices/billingTypeSlice"
 import Link from "next/link";
 import { createHubspotContact } from "@/app/API/integrations/hubspot/Hubspot";
 import { updateHubspotContact } from "@/app/API/integrations/hubspot/Hubspot";
+import { setDemoKnowledge } from "@/app/API/pages/get-trial";
 
 const Trial = () => {
   const router = useRouter();
   const dispatch = useDispatch();
   const [formData, setFormData] = useState({
-    urls: [],
     billing_type: "demo",
     checked: false,
   });
@@ -26,10 +26,12 @@ const Trial = () => {
     var regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     return regex.test(email);
   };
+
   const validateUrl = (url) => {
     var regex = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\/\w .-]*)*\/?$/;
     return regex.test(url);
   };
+
   const DisablingButton = () => {
     const requiredKeys = [
       "first_name",
@@ -40,19 +42,37 @@ const Trial = () => {
       "password",
       "url",
     ];
+
     const formValues = requiredKeys.some(
       (key) => !formData[key] || formData[key].trim() === ""
     );
-    const arr_values = ["urls"].every(
-      (key) => !formData[key] || formData[key].length === 0
-    );
+
+    // const arr_values = ["urls"].every(
+    //   (key) => !formData[key] || formData[key].length === 0
+    // );
+
     const isEmailValid = formData["email"]
       ? !validateEmail(formData["email"])
       : true;
+
     const isUrlValid = formData["url"] ? !validateUrl(formData["url"]) : true;
-    return arr_values || formValues || isEmailValid || isUrlValid;
+
+    const isFaqValid = !validateUrl(formData["faq_url"])
+
+    return isFaqValid || formValues || isEmailValid || isUrlValid;
+
   };
+
+  function addHttpsToUrl(url) {
+    if (!url.startsWith('http://') && !url.startsWith('https://')) {
+      return 'https://' + url;
+    }
+    return url;
+  }
+
   const SubmitTheForm = async () => {
+
+
     setLoading(true);
 
     let payloadForHubspot = {
@@ -111,6 +131,14 @@ const Trial = () => {
     const response = await submitCheckout(payload);
     if (response?.token) {
       Cookies.set("Token", response.token);
+
+      // Set demo knowledge. (basic knowledge about the customer)
+      let payloadForDemoKnowledge = {
+        main_webpage: addHttpsToUrl(formData.url),
+        faqs_webpage: addHttpsToUrl(formData.urls[0])
+      }
+      await setDemoKnowledge(payloadForDemoKnowledge, response.token)
+
       const bot = await createCheckoutBot(payload2, response.token);
       if (bot.status === 200 || bot.status === 201) {
         dispatch(editBillingType("demo"));
@@ -125,6 +153,9 @@ const Trial = () => {
       setLoading(false);
     }
   };
+
+
+
   return (
     <div className="container my-12">
       <div className="justify-center relative h-8  items-center flex my-4">
@@ -140,7 +171,7 @@ const Trial = () => {
       <div className="px-[25px] sm:px-0">
         <h1 className="text-[26px] mt-8 sm:text-[40px] md:text-[40px] lg:text-[40px] text-heading font-[500] text-center mx-auto sm:w-[38%]">
           Demo <span className="text-[#F5455C]">Deflection AI</span> now tailored{" "}
-        to your brand
+          to your brand
         </h1>
         <p className="sm:w-[40%] text-xl mt-8 text-center mx-auto">
           Deflection AI will configure a beautiful customized bot with your
