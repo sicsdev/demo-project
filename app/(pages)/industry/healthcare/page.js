d80283793fd6d101db7a98af337ba497e555e507
gauravsharma <gaurav@usetempo.ai>
2023-11-21 17:16:41 +0530

    Industry Page add skelton

--- app/(pages)/industry/healthcare/page.js
+++ app/(pages)/industry/healthcare/page.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import HealthCare from '@/app/components/Industry-Experts/HealthCare'
 const page = () => {
+    const defaultLoaderTime = 1000;
     return (
         <>
-            <HealthCare />
+            <HealthCare defaultLoaderTime={defaultLoaderTime} />
         </>
     )
 }
--- app/components/Home/HomeComponent.js
+++ app/components/Home/HomeComponent.js
@@ -1,54 +1,38 @@
 
 import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import SkeletonLoader from "../Skeleton/Skeleton";
 
-import React from "react";
-
-
-
-
-
-const HomeComponent = () => {
-
-
+const HomeComponent = ({defaultLoaderTime = 1000}) => {
+  const [pageLoader, setPageLoader] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setPageLoader(false);
+    }, defaultLoaderTime);
+  }, []);
 
   return (
 
     <>
       <div className="bg-black text-white py-20 mb-2">
         <div className="sm:px-20 px-5 mx-auto ">
-          <p className="sm:text-2xl text-lg">Integrate with Deflection</p>
-          <p className="sm:text-[55px] text-[25px] sm:font-normal font-light sm:w-[70%] w-[100%] leading-snug mt-3 sm:mt-5">Discover  <span className="text-red">the advantages</span> of seamless integration with our platform</p>
-          <button className="text-white bg-red sm:px-[55px] px-[60px] font-semibold py-[8px] rounded-[8px] my-0 mt-16 sm:mt-20 sm:my-5" type="button"><Link href='/checkout'>Get Started</Link></button>
+          {pageLoader ? (
+            <>
+              <SkeletonLoader width={150} height={20} />
+              <SkeletonLoader className={'!w-full sm:!w-[50%]'} height={100} />
+              <SkeletonLoader className={'sm:px-[55px] px-[60px] font-semibold py-[8px] rounded-[8px] my-0 mt-16 sm:mt-20 sm:my-5'} width={150} height={30} />
+            </>
+          ) :
+            (
+              <>
+                <p className="sm:text-2xl text-lg">Integrate with Deflection</p>
+                <p className="sm:text-[55px] text-[25px] sm:font-normal font-light sm:w-[70%] w-[100%] leading-snug mt-3 sm:mt-5">Discover  <span className="text-red">the advantages</span> of seamless integration with our platform</p>
+                <button className="text-white bg-red sm:px-[55px] px-[60px] font-semibold py-[8px] rounded-[8px] my-0 mt-16 sm:mt-20 sm:my-5" type="button"><Link href='/checkout'>Get Started</Link></button>
+              </>
+            )}
+
         </div>
       </div>
-
-
-      {/* 
-      <footer className=" bg-[#142543] shadow text-center text-white relative">
-
-        <div className="mx-auto lg:max-w-[100%]">
-
-
-          <div className="sm:p-[5rem] p-[2rem]">
-            <p className="sm:text-[29px] text-[22px] font-semibold ">Integrate with Tempo
-            </p>
-            <p className=" text-[16px] my-4">Discover the advantages of seamless integration with our platform</p>
-            <button className="text-white   w-full sm:mt-4 sm:flex sm:mx-auto justify-center hover:text-heading my-5 text-center sm:my-0  text-lg font-semibold dark:focus:ring-yellow-900 rounded-lg">
-              <Link
-                href="/checkout"
-                className=" px-[20px] py-[5px] rounded-[25px] bg-[#fe9327] hover:bg-black hover:text-white"
-              >
-                Get Started
-              </Link>
-            </button>
-          </div>
-
-
-
-        </div>
-
-      </footer> */}
-
     </>
 
   );
