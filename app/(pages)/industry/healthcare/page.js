77082175dfadd890c22ede5ea4faa2dcf299f69f
gauravsharma <gaurav@usetempo.ai>
2023-11-21 17:22:02 +0530

    Industry Page add skelton

--- app/(pages)/industry/healthcare/page.js
+++ app/(pages)/industry/healthcare/page.js
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import HealthCare from '@/app/components/Industry-Experts/HealthCare'
 const page = () => {
--- app/components/Home/HomeComponent.js
+++ app/components/Home/HomeComponent.js
@@ -1,4 +1,4 @@
-
+"use client"
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import SkeletonLoader from "../Skeleton/Skeleton";
