96fee250a55c6a416a4fc8bd44d3e87ecb5473d9
zkcastTMP <134722001+zkcastTMP@users.noreply.github.com>
2023-11-27 21:31:24 +0100

    add skeleton

--- app/components/Workflows/WorkflowBuilder/WorkflowCard.js
+++ app/components/Workflows/WorkflowBuilder/WorkflowCard.js
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import { capitalizeFirstLetter } from '../../helper/firstLetterCapital';
 import { ArrowUturnLeftIcon } from '@heroicons/react/24/outline';
 import { useState } from 'react';
+import SkeletonLoader from '../../Skeleton/Skeleton';
 
 const WorkflowCard = ({ manageData, item, key, loading, data, isAuthorizedUser }) => {
     const router = useRouter()
