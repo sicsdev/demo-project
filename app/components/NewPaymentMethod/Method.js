89e59a5789c4a8b1a968cbfc170031eef8dc5687
zkcastTMP <134722001+zkcastTMP@users.noreply.github.com>
2023-12-18 15:48:56 -0300

    Fix: Stripe wrapper (now wrapping QuickStart instead just the Element component)

--- app/components/NewPaymentMethod/Method.js
+++ app/components/NewPaymentMethod/Method.js
@@ -13,16 +13,12 @@ const Method = ({ billingState }) => {
     const userData = useSelector((state) => state.user)
 
     const setBillingValueAfterSubmit = async () => {
-
         // Update plan in Slack channel
         let payloadForSlack = { channel_id: userData?.data?.enterprise?.slack_channel_id }
         await removeTrialFromSlack(payloadForSlack)
-
         // Change account type in db
         const response = await createEnterpriseAccount({ billing_type: "normal" })
-
         dispatch(editBillingType("normal"))
-
     }
 
     const [skeltonLoading, setSkeltonLoading] = useState(true);
@@ -34,33 +30,18 @@ const Method = ({ billingState }) => {
     }, [])
 
     return (
-
-
         <>
-
             <div className='flex justify-center'>
                 <div className='w-full mx-5'>
-                    <StripeWrapper>
-                        <span className="px-2 text-xs sm:text-lg mb-4 text-center !font-semibold bg-sidebar-hover text-white py-2 rounded-md flex gap-2 justify-center items-center">
-                            {skeltonLoading ?
-                                <SkeletonLoader count={1} height={30} width={30} />
-                                :
-                                <CreditCardIcon className='h-5 w-5'></CreditCardIcon>
-                            }
-                            {skeltonLoading ?
-                                <SkeletonLoader count={1} height={30} width={300} />
-                                :
-                                "Enter payment Information to unlock all features"
-                            }
-                        </span>
-                        <BillingNew setBillingValueAfterSubmit={setBillingValueAfterSubmit} />
-                    </StripeWrapper>
+                    <span className="px-2 text-xs sm:text-lg mb-4 text-center !font-semibold bg-sidebar-hover text-white py-2 rounded-md flex gap-2 justify-center items-center">
+                        <CreditCardIcon className='h-5 w-5'></CreditCardIcon>
+                        Enter payment Information to unlock all features
+                    </span>
+                    <BillingNew setBillingValueAfterSubmit={setBillingValueAfterSubmit} />
                     <div className='border-b border-lowgray pt-5'></div>
                 </div>
             </div>
-
         </>
-
     )
 }
 
