269c44616a385a6b81a3e6cc7151c2341fa4e189
joywithyash <yash@usetempo.ai>
2023-12-18 20:14:10 +0530

    help

--- app/components/Help/Resources.js
+++ app/components/Help/Resources.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { createClient } from "contentful";
 import Link from 'next/link';
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import { ArrowRightIcon,ArrowDownRightIcon  } from "@heroicons/react/24/outline";
 import Image from 'next/image';
 import HomeComponent from '@/app/components/Home/HomeComponent';
 
@@ -16,13 +16,16 @@ const Resources = () => {
     const [showAll, setShowAll] = useState([]);
     const [showAllArticles, setShowAllArticles] = useState([]);
     const [showAllIntegrations, setShowAllIntegrations] = useState([]);
-    const [modal, setModal] = useState([]);
+    const [delay, setDelay] = useState(false);
+    const[full, setFull] = useState([])
 
     useEffect(() => {
         getAllData();
         getAllIntegrations();
-        gatAllArticles()
+        getAllArticles()
+
     }, [])
+
 
 
     const getAllData = () => {
@@ -42,30 +45,42 @@ const Resources = () => {
             content_type: "integrations",
             order: "sys.id",
         });
-
         setShowAllIntegrations(entry.items)
-
     }
 
-    const gatAllArticles = async () => {
+    const getAllArticles = async () => {
         const entry = await client.getEntries({
             content_type: "articles",
             order: "sys.id",
         });
         console.log("articles", entry.items)
         setShowAllArticles(entry.items);
+        
     }
+
     const handleAllResources = () => {
+        setDelay(true);
         setAll({ all: true, article: false, integrations: false });
+        setTimeout(() => {
+            setDelay(false);
+        }, 200);
     }
 
     const handleArticles = () => {
+        setDelay(true);
         setAll({ all: false, article: true, integrations: false });
+        setTimeout(() => {
+            setDelay(false);
+        }, 200);
     }
-
     const handleintegrations = () => {
+        setDelay(true);
         setAll({ all: false, article: false, integrations: true });
+        setTimeout(() => {
+            setDelay(false);
+        }, 200);
     }
+    
 
 
     const removeSpacesAndHyphens = (slug) => {
@@ -82,78 +97,78 @@ const Resources = () => {
                 <div className={All.integrations ? 'cursor-[pointer] border-b-[3px] border-[#f5455c]  pb-[8px] px-[22px] text-[14px] sm:text-base' : 'cursor-[pointer]  pb-[8px] px-[22px] text-[14px] sm:text-base'} onClick={handleintegrations}>Integrations</div>
             </div>
             <div className='mb-[4rem] bg-[#8080804d]'>
-                <div class="grid grid-cols-4 gap-8 p-[0px] sm:p-[2rem]">
 
-{All.all == true? <>{showAllArticles?.map((ele, key) =>
+                <div class={delay ? "opacity-0 "  : "grid grid-cols-4 gap-8 p-[0px] sm:p-[2rem]"}>
+                    {All.all == true ? <>{showAllArticles?.map((ele, key) =>
 
-<div className=" shadow-lg   p-5 sm:p-3 bg-[white]" key={key} >
-    <Link
-        href=
-        {`/resources/article/data?article=${removeSpacesAndHyphens(ele?.fields?.heading?.toLowerCase())}`}
-    >
-        <div className="flex flex-co relative h-[200px] w-[100%] justify-start items-center js-show-on-scroll">
-            <Image
-                fill={true}
-                src=
-                {ele?.fields?.previewImage?.fields?.file?.url == undefined ? "/tempo-preview.png" : ele?.fields?.previewImage?.fields?.file?.url}
+                        <div className=" shadow-lg   p-5 sm:p-3 bg-[white]" key={key} >
+                            <Link
+                                href=
+                                {`/resources/article/data?article=${removeSpacesAndHyphens(ele?.fields?.heading?.toLowerCase())}`}
+                            >
+                                <div className="flex flex-co relative h-[200px] w-[100%] justify-start items-center js-show-on-scroll">
+                                    <Image
+                                        fill={true}
+                                        src=
+                                        {ele?.fields?.previewImage?.fields?.file?.url == undefined ? "/tempo-preview.png" : ele?.fields?.previewImage?.fields?.file?.url}
 
-                alt="img"
-                className="w-full h-full bg-contain"
-                style={{ objectFit: "cover" }}
-            />
-        </div>
-        <div className="p-[12px]">
-            <div>
-                <p className="flex  text-base sm:text-para my-4  js-show-on-scroll">
-                    {ele.sys.contentType?.sys?.id}
+                                        alt="img"
+                                        className="w-full h-full bg-contain"
+                                        style={{ objectFit: "cover" }}
+                                    />
+                                </div>
+                                <div className="p-[12px]">
+                                    <div>
+                                        <p className="flex  text-base sm:text-para my-4  js-show-on-scroll">
+                                            {ele.sys.contentType?.sys?.id}
 
-                </p>
-                <p className="flex   text-base sm:text-para my-4  js-show-on-scroll">
-                    {ele.fields.heading}
-                </p>
+                                        </p>
+                                        <p className="flex   text-base sm:text-para my-4  js-show-on-scroll">
+                                            {ele.fields.heading}
+                                        </p>
 
-            </div>
-            <div className='mt-[3rem] flex justify-end'>   <ArrowRightIcon className="h-6 w-6 text-gray-500" /></div>
+                                    </div>
+                                    <div className='mt-[3rem] flex justify-end'>   <ArrowRightIcon className="h-6 w-6 text-gray-500" /></div>
 
-        </div>
-    </Link>
-</div>
-)}
-{showAllIntegrations?.map((ele, key) =>
+                                </div>
+                            </Link>
+                        </div>
+                    )}
+                        {showAllIntegrations?.map((ele, key) =>
 
-<div className=" shadow-lg    bg-[white]" key={key} >
-    <Link
-        href=
-        {`/integrations/data?article=${removeSpacesAndHyphens(ele.fields.heading.toLowerCase())}`}
-    >
-        <div className="flex flex-co relative h-[200px] w-[100%] justify-start items-center js-show-on-scroll">
-            <Image
-                fill={true}
-                src=
-                {ele?.fields?.previewImage?.fields?.file?.url == undefined ? "/tempo-preview.png" : ele?.fields?.previewImage?.fields?.file?.url}
+                            <div className="shadow-lg bg-[white]" key={key} >
+                                <Link
+                                    href=
+                                    {`/integrations/data?article=${removeSpacesAndHyphens(ele.fields.heading.toLowerCase())}`}
+                                >
+                                    <div className="flex flex-co relative h-[200px] w-[100%] justify-start items-center js-show-on-scroll">
+                                        <Image
+                                            fill={true}
+                                            src=
+                                            {ele?.fields?.previewImage?.fields?.file?.url == undefined ? "/tempo-preview.png" : ele?.fields?.previewImage?.fields?.file?.url}
 
-                alt="img"
-                className="w-full h-full bg-contain"
-                style={{ objectFit: "cover" }}
-            />
-        </div>
-        <div className="p-[12px]">
-            <div>
-                <p className="flex  text-base sm:text-para my-4  js-show-on-scroll">
-                    {ele.sys.contentType.sys.id}
+                                            alt="img"
+                                            className="w-full h-full bg-contain"
+                                            style={{ objectFit: "cover" }}
+                                        />
+                                    </div>
+                                    <div className="p-[12px]">
+                                        <div>
+                                            <p className="flex  text-base sm:text-para my-4  js-show-on-scroll">
+                                                {ele.sys.contentType.sys.id}
 
-                </p>
-                <p className="flex   text-base sm:text-para my-4  js-show-on-scroll">
-                    {ele.fields.heading}
-                </p>
-                <div className='mt-[3rem] flex justify-end'>   <ArrowRightIcon className="h-6 w-6 text-gray-500" /></div>
+                                            </p>
+                                            <p className="flex   text-base sm:text-para my-4  js-show-on-scroll">
+                                                {ele.fields.heading}
+                                            </p>
+                                            <div className='mt-[3rem] flex justify-end'>   <ArrowRightIcon className="h-6 w-6 text-gray-500" /></div>
 
-            </div>
-        </div>
-    </Link>
-</div>
-)}
-</> :""}
+                                        </div>
+                                    </div>
+                                </Link>
+                            </div>
+                        )}
+                    </> : ""}
                     {All.article == true ? <>{showAllArticles?.map((ele, key) =>
 
                         <div className=" shadow-lg   p-5 sm:p-3 bg-[white]" key={key} >
@@ -175,7 +190,7 @@ const Resources = () => {
                                 <div className="p-[12px]">
                                     <div>
                                         <p className="flex  text-base sm:text-para my-4  js-show-on-scroll">
-                                            {ele.sys.contentType.sys.id}
+                                            {ele.sys.contentType?.sys?.id}
 
                                         </p>
                                         <p className="flex   text-base sm:text-para my-4  js-show-on-scroll">
