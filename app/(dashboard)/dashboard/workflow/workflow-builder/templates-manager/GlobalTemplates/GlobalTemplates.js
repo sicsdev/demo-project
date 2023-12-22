41f7375ad5d5bacdb9a2e328586bf2cbb4a73825
zkcastTMP <134722001+zkcastTMP@users.noreply.github.com>
2023-12-22 13:31:28 -0300

    1

--- app/(dashboard)/dashboard/workflow/workflow-builder/templates-manager/GlobalTemplates/GlobalTemplates.js
+++ app/(dashboard)/dashboard/workflow/workflow-builder/templates-manager/GlobalTemplates/GlobalTemplates.js
@@ -10,7 +10,7 @@ const GlobalTemplates = () => {
     const [allGlobalTemplates, setAllGlobalTemplates] = useState([])
     const [loading, setLoading] = useState(false)
     const router = useRouter()
-    
+
     useEffect(() => {
         getAllGlobalTemplates()
     }, [])
@@ -30,7 +30,6 @@ const GlobalTemplates = () => {
             return;
         }
 
-
         // Else we create it.
         let formData = {
             name: "Default_Template_Name",
@@ -44,10 +43,32 @@ const GlobalTemplates = () => {
         if (response?.data?.id) router.push('/dashboard/workflow/workflow-builder/templates-manager?templateId=' + response.data.id)
     }
 
+    const handleSearch = (e) => {
+        let targetValue = e.target.value
+        let value = targetValue ? targetValue.toLowerCase() : ''
+        if (value == '') { getAllGlobalTemplates(); return }
+        let filteredArray = allGlobalTemplates.filter(temp => temp.name.toLowerCase().startsWith(value))
+        console.log(filteredArray)
+        setAllGlobalTemplates(filteredArray)
+    }
 
     return (
         <div>
-            <div className='flex justify-end my-3'>
+            <div className='flex justify-end my-3 gap-3'>
+                <div className="relative">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                        </svg>
+                    </div>
+                    <input
+                        type="search"
+                        id="search"
+                        className="border border-border shadow-none block px-2 bg-white  rounded-md text-lg placeholder-slate-400 text-black  focus:outline-none focus:border-sky focus:ring-2 isabled:bg-slate-50 disabled:text-slate-500 w-full focus:bg-white focus:text-[12px] pl-10"
+                        placeholder="Search"
+                        onChange={(e) => { handleSearch(e) }} />
+                </div>
+
                 <Button
                     type={"button"}
                     className="inline-block rounded border border-primary bg-primary px-6 pb-2 pt-2 text-xs font-medium  leading-normal text-white disabled:shadow-none  transition duration-150 ease-in-out hover:bg-success-600 hover:shadow-[0_8px_9px_-4px_#0000ff8a] focus:bg-success-600 focus:shadow-[0_8px_9px_-4px_#0000ff8a] focus:outline-none focus:ring-0 active:bg-success-700 active:shadow-[0_8px_9px_-4px_#0000ff8a]"
