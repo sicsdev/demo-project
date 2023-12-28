f3dd53828013fe2d8908f3803764df7bae74f9f0
frank-tempo <frank@usetempo.ai>
2023-12-27 19:45:33 -0500

    Update TeamManagement.js

--- app/components/Team/TeamManagement.js
+++ app/components/Team/TeamManagement.js
@@ -132,8 +132,8 @@ const TeamManagement = ({ state, removeMember, changeRole }) => {
       cell: (row) =>
         <div className="flex gap-3 items-center">
           {row.enterprise && row.enterprise[0]?.logo ? (
-           // <div><img src={row.enterprise[0]?.logo} alt="Enterprise Logo" style={{ width: '40px', height: '40px', borderRadius: '50%' }}></img></div>) : <><div className="bg-soft-blue p-3 rounded-full text-white">YS</div></>}
-          // <div>
+            <div><img src={row.enterprise[0]?.logo} alt="Enterprise Logo" style={{ width: '40px', height: '40px', borderRadius: '50%' }}></img></div>) : <><div className="bg-soft-blue p-3 rounded-full text-white">YS</div></>}
+           <div>
             <p className="whitespace-normal text-xs font-semibold">{row?.name}</p>
             <p className="whitespace-normal text-xs">{row?.email}</p>
           </div>
