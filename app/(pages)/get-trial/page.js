cc23d76d5127743649ba161a22a85f3bcb85e070
robbietempo <135734410+robbietempo@users.noreply.github.com>
2023-12-30 12:13:24 -0500

    Update page.js

--- app/(pages)/get-trial/page.js
+++ app/(pages)/get-trial/page.js
@@ -36,10 +36,15 @@ const validateEmail = (email) => {
   return regex.test(email);
 };
 
-  const validateUrl = (url) => {
-    var regex = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\/\w .-]*)*\/?$/;
-    return regex.test(url);
-  };
+const validateUrl = (url) => {
+  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return !!pattern.test(url);
+};
 
   const DisablingButton = () => {
 
