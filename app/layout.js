4ef02a7a84fd370f36971b3f3204db8fef7fd917
joywithyash <yash@usetempo.ai>
2024-01-02 02:18:44 +0530

    gtm

--- app/layout.js
+++ app/layout.js
@@ -236,6 +236,12 @@ export default function RootLayout({ children }) {
           }}
         />
         {/* <!-- End Meta Pixel Code --> */}
+
+
+
+
+
+        
         {/* Google tag */}
         <script
           async
@@ -252,24 +258,36 @@ export default function RootLayout({ children }) {
       `,
           }}
         />
-        {/* <!-- Google Tag Manager -->  */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': 
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], 
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); 
-})(window,document,'script','dataLayer','GTM-P3BH433');`,
-          }}
-        />
-        {/* <!-- End Google Tag Manager --> */}{" "}
+       
+
+
+
+{/* <!-- Google Tag Manager --> */}
+
+<script
+dangerouslySetInnerHTML={{
+  __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })
+  (window,document,'script','dataLayer','GTM-P3BH433');`, }}/>
+{/* <!-- End Google Tag Manager --> */}
+
+
+
       </head>
 
       <body suppressHydrationWarning={true} className={inter.className}>
         <NoSSRProviderWrapper>
           <div className="tempo_container">{children}</div>
         </NoSSRProviderWrapper>
+
+        {/* <!-- Google Tag Manager (noscript) --> */}
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P3BH433"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+{/* <!-- End Google Tag Manager (noscript) --> */}
+
 
         <Script
           dangerouslySetInnerHTML={{
