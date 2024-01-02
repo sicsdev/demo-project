59a3e0bc4775b78af91f2902461bfe384967b53a
vivekTempo <vivek@usetempo.ai>
2024-01-03 00:30:56 +0530

    integration fixed

--- app/components/Integration/NewCustomIntegration.js
+++ app/components/Integration/NewCustomIntegration.js
@@ -190,6 +190,7 @@ const NewCustomIntegrations = ({ setIntegrationform, help, formData, setFormData
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="h-8 w-8 rounded-lg text-black p-2"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path></svg>
                 </div>
             </div>
+            
             <div className="sm:pt-2 pt-2">
                 <div class="grid grid-cols-1 md:grid-cols-[70%,30%] items-start">
                     <div className="">
